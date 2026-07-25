NousResearch/hermes-agent

feat(kanban): support board notify subscriptions (#40917)
Allow notify-subscribe/list/unsubscribe to target a whole board and
persist per-board event filters in kanban_board_notify_subs.

Teach the gateway notifier to claim, rewind, and advance board-level
cursors so one chat can receive completed/blocked events across a
board without replaying historical terminal events.

tests/hermes_cli/test_kanban_core_functionality.py
@@ -868,6 +868,50 @@ def test_cli_notify_subscribe_and_list(kanban_home):
     assert "Unsubscribed" in rm
 
 
+def test_cli_board_notify_subscribe_and_list(kanban_home):
+    run_slash("boards create alpha")
+    out = run_slash(
+        "notify-subscribe --board alpha --platform telegram "
+        "--chat-id 999 --kinds completed,blocked",
+    )
+    assert "Subscribed" in out
+    assert "board alpha" in out
+    lst = run_slash("notify-list --board alpha --json")
+    subs = json.loads(lst)
+    board_subs = [s for s in subs if s.get("scope") == "board"]
+    assert len(board_subs) == 1
+    assert board_subs[0]["platform"] == "telegram"
+    assert board_subs[0]["chat_id"] == "999"
+    assert board_subs[0]["kinds"] == ["completed", "blocked"]
+    rm = run_slash(
+        "notify-unsubscribe --board alpha --platform telegram --chat-id 999",
+    )
+    assert "Unsubscribed" in rm
+
+
+def test_board_notify_subscribe_starts_from_current_cursor(kanban_home):
+    conn = kb.connect()
+    try:
+        tid = kb.create_task(conn, title="historical")
+        kb.complete_task(conn, tid, summary="already done")
+        kb.add_board_notify_sub(
+            conn,
+            platform="telegram",
+            chat_id="chat-1",
+            kinds=["completed"],
+        )
+        cursor, events = kb.unseen_events_for_board_sub(
+            conn,
+            platform="telegram",
+            chat_id="chat-1",
+            kinds=["completed"],
+        )
+    finally:
+        conn.close()
+    assert cursor >= 1
+    assert events == []
+
+
 def test_cli_log_missing_task(kanban_home):
     # No such task → exit-style (no log for...) message on stderr, returned
     # in combined output.

Carrie111998/Hermes

tests/hermes_cli/test_kanban_core_functionality.py
@@ -868,6 +868,60 @@ def test_cli_notify_subscribe_and_list(kanban_home):
     assert "Unsubscribed" in rm
 
 
+def test_cli_board_notify_commands_target_explicit_board_not_active_board(kanban_home):
+    run_slash("boards create alpha")
+    run_slash("boards create beta")
+    run_slash("boards switch alpha")
+
+    out = run_slash(
+        "notify-subscribe --board beta --platform telegram "
+        "--chat-id 999 --kinds completed,blocked",
+    )
+    assert "Subscribed" in out
+    assert "board beta" in out
+
+    active_subs = json.loads(run_slash("notify-list --board alpha --json"))
+    assert active_subs == []
+
+    subs = json.loads(run_slash("notify-list --board beta --json"))
+    board_subs = [s for s in subs if s.get("scope") == "board"]
+    assert len(board_subs) == 1
+    assert board_subs[0]["board"] == "beta"
+    assert board_subs[0]["platform"] == "telegram"
+    assert board_subs[0]["chat_id"] == "999"
+    assert board_subs[0]["kinds"] == ["completed", "blocked"]
+
+    rm = run_slash(
+        "notify-unsubscribe --board beta --platform telegram --chat-id 999",
+    )
+    assert "Unsubscribed" in rm
+    assert "board beta" in rm
+    assert json.loads(run_slash("notify-list --board beta --json")) == []
+
+
+def test_board_notify_subscribe_starts_from_current_cursor(kanban_home):
+    conn = kb.connect()
+    try:
+        tid = kb.create_task(conn, title="historical")
+        kb.complete_task(conn, tid, summary="already done")
+        kb.add_board_notify_sub(
+            conn,
+            platform="telegram",
+            chat_id="chat-1",
+            kinds=["completed"],
+        )
+        cursor, events = kb.unseen_events_for_board_sub(
+            conn,
+            platform="telegram",
+            chat_id="chat-1",
+            kinds=["completed"],
+        )
+    finally:
+        conn.close()
+    assert cursor >= 1
+    assert events == []
+
+
 def test_cli_log_missing_task(kanban_home):
     # No such task → exit-style (no log for...) message on stderr, returned
     # in combined output.