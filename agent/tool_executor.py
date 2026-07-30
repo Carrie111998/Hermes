NousResearch/hermes-agent

fix(memory): skip same-state quota retries

agent/tool_executor.py
@@ -136,6 +136,24 @@ def _ra():
     return run_agent
 
 
+def _memory_current_state_token(agent, function_name: str, function_args: dict) -> str | None:
+    if function_name != "memory":
+        return None
+    memory_store = getattr(agent, "_memory_store", None)
+    if memory_store is None or not hasattr(memory_store, "stat"):
+        return None
+    store = function_args.get("store") or function_args.get("target") or "memory"
+    try:
+        stat = memory_store.stat(store)
+    except Exception:
+        return None
+    if isinstance(stat, dict):
+        token = stat.get("store_state_token")
+        if isinstance(token, str):
+            return token
+    return None
+
+
 def _is_interpreter_shutdown_submit_error(exc: RuntimeError) -> bool:
     return "cannot schedule new futures after interpreter shutdown" in str(exc)
 
@@ -474,7 +492,13 @@ def execute_tool_calls_concurrent(agent, assistant_message, messages: list, effe
                     middleware_trace=list(middleware_trace),
                 )
             else:
-                guardrail_decision = agent._tool_guardrails.before_call(function_name, function_args)
+                guardrail_decision = agent._tool_guardrails.before_call(
+                    function_name,
+                    function_args,
+                    current_store_state_token=_memory_current_state_token(
+                        agent, function_name, function_args
+                    ),
+                )
                 if not guardrail_decision.allows_execution:
                     block_result = agent._guardrail_block_result(guardrail_decision)
                     blocked_by_guardrail = True
@@ -1119,7 +1143,13 @@ def execute_tool_calls_sequential(agent, assistant_message, messages: list, effe
 
         _guardrail_block_decision: ToolGuardrailDecision | None = None
         if _block_msg is None:
-            guardrail_decision = agent._tool_guardrails.before_call(function_name, function_args)
+            guardrail_decision = agent._tool_guardrails.before_call(
+                function_name,
+                function_args,
+                current_store_state_token=_memory_current_state_token(
+                    agent, function_name, function_args
+                ),
+            )
             if not guardrail_decision.allows_execution:
                 _guardrail_block_decision = guardrail_decision
 

Carrie111998/Hermes

agent/tool_executor.py
@@ -165,6 +165,24 @@ def _ra():
     return run_agent
 
 
+def _memory_current_state_token(agent, function_name: str, function_args: dict) -> str | None:
+    if function_name != "memory":
+        return None
+    memory_store = getattr(agent, "_memory_store", None)
+    if memory_store is None or not hasattr(memory_store, "stat"):
+        return None
+    store = function_args.get("store") or function_args.get("target") or "memory"
+    try:
+        stat = memory_store.stat(store)
+    except Exception:
+        return None
+    if isinstance(stat, dict):
+        token = stat.get("store_state_token")
+        if isinstance(token, str):
+            return token
+    return None
+
+
 def _is_interpreter_shutdown_submit_error(exc: RuntimeError) -> bool:
     return "cannot schedule new futures after interpreter shutdown" in str(exc)
 
@@ -440,7 +458,11 @@ def _run_agent_tool_execution_middleware(
         guardrail_decision = None
         if block_message is None:
             guardrail_decision = agent._tool_guardrails.before_call(
-                function_name, final_args
+                function_name,
+                final_args,
+                current_store_state_token=_memory_current_state_token(
+                    agent, function_name, final_args
+                ),
             )
             if guardrail_decision.allows_execution:
                 guardrail_decision = None