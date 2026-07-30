NousResearch/hermes-agent

feat(gateway): split SUPPORTS_STREAM_EDITING from SUPPORTS_MESSAGE_EDITING for qqbot
QQBot can edit an in-flight C2C stream bubble (same stream_msg_id) but
cannot edit arbitrary already-sent message IDs. Conflating the two flags
meant either C2C streaming was disabled or tool-progress bubbles were
emitted as noisy duplicate messages on group/guild paths.

- base: add SUPPORTS_STREAM_EDITING property, defaults to
  SUPPORTS_MESSAGE_EDITING so existing adapters are unchanged.
- run: stream consumer now gates on SUPPORTS_STREAM_EDITING; tool-progress
  editing gates on generic edit_message override AND SUPPORTS_MESSAGE_EDITING,
  so QQ (stream-only editor) correctly skips progress bubbles.
- qqbot: advertise stream editing while keeping generic message editing off.
- docs/config example: document group_sessions_per_user sender prefix.
- tests: cover the stream-vs-generic editing gates and group context buffer.

Note: QQ group messages only carry member_openid (no readable nickname);
the platform does not provide sender display names, so no sender-name
enrichment is added on that path.

gateway/run.py
@@ -3546,11 +3546,42 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                     _profile = get_active_profile_name() or "default"
                 except Exception:
                     _profile = None
+        _group_spu, _thread_spu = self._effective_session_sharing(source)
         return build_session_key(
             source,
-            group_sessions_per_user=getattr(config, "group_sessions_per_user", True),
-            thread_sessions_per_user=getattr(config, "thread_sessions_per_user", False),
+            group_sessions_per_user=_group_spu,
+            thread_sessions_per_user=_thread_spu,
             profile=_profile,
+        )
+
+    def _effective_session_sharing(self, source: SessionSource) -> tuple[bool, bool]:
+        """Resolve the per-platform effective (group, thread) session-sharing flags.
+
+        Single source of truth shared by session-key construction and sender
+        attribution. Mirrors the values ``BasePlatformAdapter`` uses to build
+        the session key (``self.config.extra.get(...)``): a per-platform
+        override (e.g. QQ ``platforms.qqbot.extra.group_sessions_per_user:
+        false``) therefore drives BOTH the shared key namespace AND the
+        ``[user_name]`` sender prefix in ``_prepare_inbound_message_text``.
+
+        Without this, attribution read only the global ``GatewayConfig`` value,
+        so a QQ-only shared session merged different users into one session
+        with no sender prefix.
+        """
+        config = getattr(self, "config", None)
+        group_default = getattr(config, "group_sessions_per_user", True)
+        thread_default = getattr(config, "thread_sessions_per_user", False)
+        platform_cfg = None
+        try:
+            platform_cfg = config.platforms.get(source.platform)
+        except Exception:
+            platform_cfg = None
+        extra = getattr(platform_cfg, "extra", None) if platform_cfg is not None else None
+        if not isinstance(extra, dict):
+            return (group_default, thread_default)
+        return (
+            extra.get("group_sessions_per_user", group_default),
+            extra.get("thread_sessions_per_user", thread_default),
         )
 
     def _telegram_topic_mode_enabled(self, source: SessionSource) -> bool:
@@ -10452,8 +10483,12 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
         """
         history = history or []
         message_text = event.text or ""
-        _group_sessions_per_user = getattr(self.config, "group_sessions_per_user", True)
-        _thread_sessions_per_user = getattr(self.config, "thread_sessions_per_user", False)
+        # Resolve sharing flags via the same per-platform effective value used
+        # to build the session key (see _effective_session_sharing). Using the
+        # global GatewayConfig here would desync attribution from the key: a
+        # QQ-only shared session (extra.group_sessions_per_user=false) would
+        # merge users into one key yet skip the [user_name] sender prefix.
+        _group_sessions_per_user, _thread_sessions_per_user = self._effective_session_sharing(source)
         # Prefer the already resolved session key from the caller so this write
         # key matches the consume key at the run_conversation site. Fall back
         # to deriving it here for tests and legacy standalone callers.
@@ -16930,7 +16965,11 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                             _chat_id=source.chat_id,
                         ) -> None:
                             _adapter.pause_typing_for_chat(_chat_id)
-                    _adapter_supports_edit = getattr(_adapter, "SUPPORTS_MESSAGE_EDITING", True)
+                    _adapter_supports_edit = getattr(
+                        _adapter,
+                        "SUPPORTS_STREAM_EDITING",
+                        getattr(_adapter, "SUPPORTS_MESSAGE_EDITING", True),
+                    )
                     _effective_cursor = _scfg.cursor if _adapter_supports_edit else ""
                     _buffer_only = False
                     if source.platform == Platform.MATRIX:
@@ -17736,10 +17775,19 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
             if not adapter:
                 return
 
-            # Skip tool progress for platforms that don't support message
-            # editing (e.g. iMessage/BlueBubbles) — each progress update
-            # would become a separate message bubble, which is noisy.
-            if type(adapter).edit_message is BasePlatformAdapter.edit_message:
+            # Skip tool progress for platforms that don't support editing an
+            # arbitrary already-sent message (e.g. iMessage/BlueBubbles, and
+            # QQBot which only edits in-flight C2C stream handles) — each
+            # progress update would otherwise become a separate message
+            # bubble, which is noisy.  Two gates:
+            #   * adapter never overrides edit_message (generic non-editable), or
+            #   * adapter advertises SUPPORTS_MESSAGE_EDITING = False (it may
+            #     override edit_message only for stream handles, like QQBot).
+            _generic_editing = (
+                type(adapter).edit_message is not BasePlatformAdapter.edit_message
+                and getattr(adapter, "SUPPORTS_MESSAGE_EDITING", True)
+            )
+            if not _generic_editing:
                 while not progress_queue.empty():
                     try:
                         progress_queue.get_nowait()
@@ -18267,7 +18315,11 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                         # without edit support, the consumer sends a partial
                         # first message that can never be updated, resulting in
                         # duplicate messages (partial + final).
-                        _adapter_supports_edit = getattr(_adapter, "SUPPORTS_MESSAGE_EDITING", True)
+                        _adapter_supports_edit = getattr(
+                            _adapter,
+                            "SUPPORTS_STREAM_EDITING",
+                            getattr(_adapter, "SUPPORTS_MESSAGE_EDITING", True),
+                        )
                         if not _adapter_supports_edit:
                             raise RuntimeError("skip streaming for non-editable platform")
                         _effective_cursor = _scfg.cursor

Carrie111998/Hermes

gateway/run.py
@@ -3621,8 +3621,16 @@ class TurnRunner:
         # getattr, not attribute access: duck-typed adapters (test fakes,
         # minimal plugin adapters) may not define edit_message at all —
         # "missing" means the same thing as "base no-op": can't edit.
+        # The SUPPORTS_MESSAGE_EDITING check is also required: an adapter may
+        # override edit_message for in-flight stream handles only (QQBot), so
+        # overriding it does NOT imply arbitrary sent-message editing works.
         _adapter_edit = getattr(type(adapter), "edit_message", None)
-        if _adapter_edit is None or _adapter_edit is BasePlatformAdapter.edit_message:
+        _generic_editing = (
+            _adapter_edit is not None
+            and _adapter_edit is not BasePlatformAdapter.edit_message
+            and getattr(adapter, "SUPPORTS_MESSAGE_EDITING", True)
+        )
+        if not _generic_editing:
             while not ctx.progress_queue.empty():
                 try:
                     ctx.progress_queue.get_nowait()
@@ -6322,11 +6330,42 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                     _profile = get_active_profile_name() or "default"
                 except Exception:
                     _profile = None
+        _group_spu, _thread_spu = self._effective_session_sharing(source)
         return build_session_key(
             source,
-            group_sessions_per_user=getattr(config, "group_sessions_per_user", True),
-            thread_sessions_per_user=getattr(config, "thread_sessions_per_user", False),
+            group_sessions_per_user=_group_spu,
+            thread_sessions_per_user=_thread_spu,
             profile=_profile,
+        )
+
+    def _effective_session_sharing(self, source: SessionSource) -> tuple[bool, bool]:
+        """Resolve the per-platform effective (group, thread) session-sharing flags.
+
+        Single source of truth shared by session-key construction and sender
+        attribution. Mirrors the values ``BasePlatformAdapter`` uses to build
+        the session key (``self.config.extra.get(...)``): a per-platform
+        override (e.g. QQ ``platforms.qqbot.extra.group_sessions_per_user:
+        false``) therefore drives BOTH the shared key namespace AND the
+        ``[user_name]`` sender prefix in ``_prepare_inbound_message_text``.
+
+        Without this, attribution read only the global ``GatewayConfig`` value,
+        so a QQ-only shared session merged different users into one session
+        with no sender prefix.
+        """
+        config = getattr(self, "config", None)
+        group_default = getattr(config, "group_sessions_per_user", True)
+        thread_default = getattr(config, "thread_sessions_per_user", False)
+        platform_cfg = None
+        try:
+            platform_cfg = config.platforms.get(source.platform)
+        except Exception:
+            platform_cfg = None
+        extra = getattr(platform_cfg, "extra", None) if platform_cfg is not None else None
+        if not isinstance(extra, dict):
+            return (group_default, thread_default)
+        return (
+            extra.get("group_sessions_per_user", group_default),
+            extra.get("thread_sessions_per_user", thread_default),
         )
 
     def _telegram_topic_mode_enabled(self, source: SessionSource) -> bool:
@@ -15017,8 +15056,12 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
             if _pending_stt_prepared
             else event.text
         ) or ""
-        _group_sessions_per_user = getattr(self.config, "group_sessions_per_user", True)
-        _thread_sessions_per_user = getattr(self.config, "thread_sessions_per_user", False)
+        # Resolve sharing flags via the same per-platform effective value used
+        # to build the session key (see _effective_session_sharing). Using the
+        # global GatewayConfig here would desync attribution from the key: a
+        # QQ-only shared session (extra.group_sessions_per_user=false) would
+        # merge users into one key yet skip the [user_name] sender prefix.
+        _group_sessions_per_user, _thread_sessions_per_user = self._effective_session_sharing(source)
         # Prefer the already resolved session key from the caller so this write
         # key matches the consume key at the run_conversation site. Fall back
         # to deriving it here for tests and legacy standalone callers.
@@ -22625,9 +22668,10 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
         """Build the shared ``StreamConsumerConfig`` and the optional
         Telegram pause-typing closure used by both agent-run paths.
 
-        ``on_missing_cursor`` controls how platforms whose adapter sets
-        ``SUPPORTS_MESSAGE_EDITING = False`` are handled — both semantics
-        are preserved verbatim from the pre-refactor call sites:
+        ``on_missing_cursor`` controls how platforms whose adapter cannot
+        edit an in-flight streaming bubble (``SUPPORTS_STREAM_EDITING``,
+        falling back to ``SUPPORTS_MESSAGE_EDITING``) are handled — both
+        semantics are preserved verbatim from the pre-refactor call sites:
 
         - ``"fallback"`` (proxy path): stream anyway with an empty cursor.
         - ``"raise"`` (in-process agent path): raise ``RuntimeError`` so
@@ -22651,7 +22695,11 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
         # duplicate messages (partial + final).
         # (The proxy path instead opts into a cursorless fallback
         # via on_missing_cursor="fallback".)
-        _adapter_supports_edit = getattr(adapter, "SUPPORTS_MESSAGE_EDITING", True)
+        _adapter_supports_edit = getattr(
+            adapter,
+            "SUPPORTS_STREAM_EDITING",
+            getattr(adapter, "SUPPORTS_MESSAGE_EDITING", True),
+        )
         if not _adapter_supports_edit and on_missing_cursor == "raise":
             raise RuntimeError("skip streaming for non-editable platform")
         _effective_cursor = scfg.cursor if _adapter_supports_edit else ""