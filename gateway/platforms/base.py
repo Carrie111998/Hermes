AnandaAnugrahHandyanto/savarez_agent

fix(telegram): stop typing after final reply

gateway/platforms/base.py
@@ -3665,6 +3665,13 @@ class BasePlatformAdapter(ABC):
                         _thread_metadata["notify"] = True
                     else:
                         _thread_metadata = {"notify": True}
+                    # Telegram re-triggers typing after each send() to keep the
+                    # bubble alive across intermediate progress/status messages.
+                    # Mark the terminal reply so adapters can skip that post-send
+                    # refresh; otherwise users can receive the final answer while
+                    # the client still shows Hermes as typing for a few extra
+                    # seconds.
+                    _thread_metadata["suppress_post_send_typing"] = True
                     result = await self._send_with_retry(
                         chat_id=event.source.chat_id,
                         content=text_content,

Carrie111998/Hermes

gateway/platforms/base.py
@@ -6022,6 +6022,14 @@ class BasePlatformAdapter(ABC):
                         event.source.chat_id,
                     )
                     _reply_anchor = _reply_anchor_for_event(event)
+                    # Telegram re-triggers typing after each send() to keep the
+                    # bubble alive across intermediate progress/status messages.
+                    # Mark the terminal reply so adapters can skip that post-send
+                    # refresh; otherwise users can receive the final answer while
+                    # the client still shows Hermes as typing for a few extra
+                    # seconds.
+                    _final_thread_metadata["suppress_post_send_typing"] = True
+
                     # Delivery-obligation ledger: durably record the final
                     # response BEFORE the send attempt so a gateway crash
                     # between finalize and platform ACK can redeliver it on