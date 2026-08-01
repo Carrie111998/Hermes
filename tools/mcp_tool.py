NousResearch/hermes-agent

feat: per-MCP-server tool_injection control (description_only mode)
Adds tool_injection config option for MCP servers, supporting two modes:
- full (default, backwards-compatible): all tools loaded eagerly
- description_only: tools always deferred, discovered via tool_search

Modeled after Claude Code's defer_loading pattern and Hermes's
own Skill frontmatter discovery (name+desc in prompt, body on demand).

Changes:
- tools/tool_search.py: mark_description_only_tool() registry + force-defer
  in assemble_tools() even when under threshold
- tools/mcp_tool.py: read tool_injection config, mark tools during registration
- agent/system_prompt.py: inject MCP tool inventory (name+desc) into stable tier
  for description-only servers, mirroring the Skill index block

Closes #66736
Refs: #6839 (Lazy Tool Schema Loading), Writer Harness paper (arXiv 2607.06906)

tools/mcp_tool.py
@@ -4759,6 +4759,18 @@ def _register_server_tools(name: str, server: MCPServerTask, config: dict) -> Li
     include_set = _normalize_name_filter(tools_filter.get("include"), f"mcp_servers.{name}.tools.include")
     exclude_set = _normalize_name_filter(tools_filter.get("exclude"), f"mcp_servers.{name}.tools.exclude")
 
+    # Description-only mode: tools from this server are always deferrable,
+    # discovered via tool_search rather than loaded into the tools array
+    # on every turn. Mirroring Claude Code's ``defer_loading`` pattern.
+    tool_injection = config.get("tool_injection", "full")
+    is_description_only = tool_injection == "description_only"
+
+    if is_description_only:
+        logger.info(
+            "MCP server '%s': tool_injection=description_only — "
+            "tools will be deferred and discovered via tool_search", name,
+        )
+
     def _should_register(tool_name: str) -> bool:
         if include_set:
             return tool_name in include_set
@@ -4798,6 +4810,13 @@ def _register_server_tools(name: str, server: MCPServerTask, config: dict) -> Li
         )
         _track_mcp_tool_server(tool_name_prefixed, name)
         registered_names.append(tool_name_prefixed)
+
+        # Description-only tools: mark for always-deferred treatment
+        # so they are discovered via tool_search rather than bloating
+        # the tools array on every turn.
+        if is_description_only:
+            from tools.tool_search import mark_description_only_tool
+            mark_description_only_tool(tool_name_prefixed)
 
     # Register MCP Resources & Prompts utility tools, filtered by config and
     # only when the server actually supports the corresponding capability.

Carrie111998/Hermes

tools/mcp_tool.py
@@ -5732,6 +5732,18 @@ def _register_server_tools(name: str, server: MCPServerTask, config: dict) -> Li
         tools_filter.get("exclude"), f"mcp_servers.{name}.tools.exclude"
     )
 
+    # Description-only mode: tools from this server are always deferrable,
+    # discovered via tool_search rather than loaded into the tools array
+    # on every turn. Mirroring Claude Code's ``defer_loading`` pattern.
+    tool_injection = config.get("tool_injection", "full")
+    is_description_only = tool_injection == "description_only"
+
+    if is_description_only:
+        logger.info(
+            "MCP server '%s': tool_injection=description_only — "
+            "tools will be deferred and discovered via tool_search", name,
+        )
+
     def _should_register(tool_name: str) -> bool:
         if include_set:
             return matches_name_filter(tool_name, include_set)
@@ -5876,6 +5888,16 @@ def _register_server_tools(name: str, server: MCPServerTask, config: dict) -> Li
 
         _track_mcp_tool_server(registry_name, name)
         registered_names.append(registry_name)
+
+        # Description-only mode: mark for always-deferred treatment so tools
+        # are discovered via tool_search rather than bloating the tools array
+        # on every turn. Everything from this server flows through this one
+        # registration loop — raw MCP tools AND generated resource/prompt
+        # utility schemas — so a single mark here covers both.
+        if is_description_only:
+            from tools.tool_search import mark_description_only_tool
+
+            mark_description_only_tool(registry_name)
 
     if registered_names:
         registry.register_toolset_alias(name, toolset_name)