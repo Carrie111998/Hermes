NousResearch/hermes-agent

fix(prompt): load profile AGENTS.md from HERMES_HOME when cwd lacks it
Closes #66118

Root cause: project context discovery only looked for AGENTS.md under the
process cwd. Profile SOUL.md already comes from HERMES_HOME, but profile
AGENTS.md sitting in profiles/<name>/ (or a custom HERMES_HOME) was ignored
whenever the shell cwd was not that profile directory — typical of
`hermes -p <profile> -z` in containers/K8s and non-home workdirs.

Fix: after cwd AGENTS.md, fall back to $HERMES_HOME/AGENTS.md. Cwd still
wins so a real project AGENTS.md stays authoritative.

Verification: pytest tests/agent/test_prompt_builder.py -k agents_md (10 passed)

agent/prompt_builder.py
@@ -1887,21 +1887,40 @@ def _load_hermes_md(cwd_path: Path, context_length: Optional[int] = None) -> str
 
 
 def _load_agents_md(cwd_path: Path, context_length: Optional[int] = None) -> str:
-    """AGENTS.md — top-level only (no recursive walk)."""
-    for name in ["AGENTS.md", "agents.md"]:
-        candidate = cwd_path / name
-        if candidate.exists():
-            try:
-                content = candidate.read_text(encoding="utf-8").strip()
-                if content:
-                    content = _scan_context_content(content, name)
-                    result = f"## {name}\n\n{content}"
-                    return _truncate_content(
-                        result, "AGENTS.md", context_length=context_length,
-                        read_path=str(candidate),
-                    )
-            except Exception as e:
-                logger.debug("Could not read %s: %s", candidate, e)
+    """AGENTS.md — prefer process cwd / project root, then HERMES_HOME.
+
+    Top-level only (no recursive walk). Project cwd wins so a repo's own
+    AGENTS.md remains authoritative when present. When the process cwd is
+    *not* the active profile (common for ``hermes -p <profile> -z …`` launched
+    from /, /tmp, or a container workdir that is not HERMES_HOME), fall back to
+    ``$HERMES_HOME/AGENTS.md`` so profile-level agent instructions still load
+    (#66118) — matching how ``SOUL.md`` is read from HERMES_HOME independently
+    of cwd.
+    """
+    search_dirs: list[Path] = [cwd_path]
+    try:
+        home = get_hermes_home().resolve()
+        if home not in search_dirs and home != cwd_path.resolve():
+            search_dirs.append(home)
+    except Exception as e:
+        logger.debug("Could not resolve HERMES_HOME for AGENTS.md fallback: %s", e)
+
+    for directory in search_dirs:
+        for name in ["AGENTS.md", "agents.md"]:
+            candidate = directory / name
+            if candidate.exists():
+                try:
+                    content = candidate.read_text(encoding="utf-8").strip()
+                    if content:
+                        content = _scan_context_content(content, name)
+                        # Label uses basename only — path is for read tracing.
+                        result = f"## {name}\n\n{content}"
+                        return _truncate_content(
+                            result, "AGENTS.md", context_length=context_length,
+                            read_path=str(candidate),
+                        )
+                except Exception as e:
+                    logger.debug("Could not read %s: %s", candidate, e)
     return ""
 
 

Carrie111998/Hermes

agent/prompt_builder.py
@@ -2206,6 +2206,12 @@ def _load_agents_md(cwd_path: Path, context_length: Optional[int] = None) -> str
     deduplicated.  With a single match — the common case, and always the
     case outside a git repo — output is identical to the historical
     single-file behavior.
+
+    When the chain yields nothing, fall back to ``$HERMES_HOME/AGENTS.md``
+    so profile-level agent instructions still load when the process cwd is
+    not the active profile (#66118) — matching how ``SOUL.md`` is read from
+    HERMES_HOME independently of cwd. Project / cwd chain content always
+    wins when present.
     """
     cwd_resolved = cwd_path.resolve()
     sections: List[str] = []
@@ -2238,6 +2244,30 @@ def _load_agents_md(cwd_path: Path, context_length: Optional[int] = None) -> str
             sections.append(section)
             break  # first name match wins per directory
     if not sections:
+        # Profile-level fallback when no project AGENTS.md exists (#66118).
+        try:
+            home = get_hermes_home().resolve()
+        except Exception as e:
+            logger.debug("Could not resolve HERMES_HOME for AGENTS.md fallback: %s", e)
+            return ""
+        if home != cwd_resolved:
+            for name in ["AGENTS.md", "agents.md"]:
+                candidate = home / name
+                if not candidate.exists():
+                    continue
+                try:
+                    content = candidate.read_text(encoding="utf-8").strip()
+                except Exception as e:
+                    logger.debug("Could not read %s: %s", candidate, e)
+                    continue
+                if not content:
+                    continue
+                scanned = _scan_context_content(content, name)
+                section = f"## {name}\n\n{scanned}"
+                return _truncate_content(
+                    section, "AGENTS.md", context_length=context_length,
+                    read_path=str(candidate),
+                )
         return ""
     if len(sections) == 1:
         return sections[0]