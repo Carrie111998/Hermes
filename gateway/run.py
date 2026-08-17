NousResearch/hermes-agent

fix(gateway): stop standing-goal loop after a failed provider turn
A provider/model failure is normalized into non-empty user-facing text by
_normalize_empty_agent_response(). The post-turn goal-continuation hook gated
only on _final_text.strip(), so that normalized error text passed the gate: the
judge answered "continue" and re-enqueued the same synthetic
[Continuing toward your standing goal] prompt, looping on the failure and
appending duplicate continuation blocks until the session grew without bound.

Add _should_continue_goal_after_turn(agent_result, final_text), which requires
non-empty text AND the same success semantics as
_should_clear_resume_pending_after_turn (rejecting interrupted/failed/partial/
error/completed=False), and gate the continuation on it. Bare string results
keep the legacy text-only behaviour.

Fixes #63180

gateway/run.py
@@ -2686,6 +2686,28 @@ def _should_clear_resume_pending_after_turn(agent_result: dict) -> bool:
     return True
 
 
+def _should_continue_goal_after_turn(agent_result, final_text) -> bool:
+    """Return True only when a standing goal may be continued after this turn.
+
+    A continuation must be gated on *real* progress. ``final_text`` alone is not
+    enough: ``_normalize_empty_agent_response()`` deliberately returns non-empty,
+    user-facing text for ``failed``/``partial``/interrupted/errored results, so a
+    provider failure still yields a non-empty final response. Feeding that to the
+    goal judge makes it answer "continue", which re-enqueues the same synthetic
+    ``[Continuing toward your standing goal]`` prompt and loops on the failure.
+
+    Require non-empty text *and* the same success semantics used by
+    ``_should_clear_resume_pending_after_turn`` so only genuinely completed turns
+    advance the goal. Non-dict results (bare string replies) are treated as
+    successful, matching the legacy text-only behaviour.
+    """
+    if not str(final_text or "").strip():
+        return False
+    if not isinstance(agent_result, dict):
+        return True
+    return _should_clear_resume_pending_after_turn(agent_result)
+
+
 def _preserve_queued_followup_history_offset(
     current_result: dict,
     followup_result: dict,
@@ -10290,10 +10312,13 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                     _final_text = str(_agent_result.get("final_response") or "")
                 elif isinstance(_agent_result, str):
                     _final_text = _agent_result
-                # Skip for empty responses (interrupted / errored) — the
-                # judge would almost always say "continue" and we'd loop
-                # on error. Let the user drive the next turn.
-                if _final_text.strip():
+                # Skip for empty, failed, partial, or interrupted turns — a
+                # provider failure is normalized into non-empty user-facing
+                # text, and the judge would almost always say "continue", so
+                # we'd loop on the error and append duplicate continuation
+                # prompts. Require a genuinely successful turn instead; let the
+                # user drive the next one otherwise.
+                if _should_continue_goal_after_turn(_agent_result, _final_text):
                     try:
                         session_entry = await self.async_session_store.get_or_create_session(source)
                     except Exception:

Carrie111998/Hermes

gateway/run.py
@@ -3901,6 +3901,28 @@ def _should_clear_resume_pending_after_turn(agent_result: dict) -> bool:
     return True
 
 
+def _should_continue_goal_after_turn(agent_result, final_text) -> bool:
+    """Return True only when a standing goal may be continued after this turn.
+
+    A continuation must be gated on *real* progress. ``final_text`` alone is not
+    enough: ``_normalize_empty_agent_response()`` deliberately returns non-empty,
+    user-facing text for ``failed``/``partial``/interrupted/errored results, so a
+    provider failure still yields a non-empty final response. Feeding that to the
+    goal judge makes it answer "continue", which re-enqueues the same synthetic
+    ``[Continuing toward your standing goal]`` prompt and loops on the failure.
+
+    Require non-empty text *and* the same success semantics used by
+    ``_should_clear_resume_pending_after_turn`` so only genuinely completed turns
+    advance the goal. Non-dict results (bare string replies) are treated as
+    successful, matching the legacy text-only behaviour.
+    """
+    if not str(final_text or "").strip():
+        return False
+    if not isinstance(agent_result, dict):
+        return True
+    return _should_clear_resume_pending_after_turn(agent_result)
+
+
 def _preserve_queued_followup_history_offset(
     current_result: dict,
     followup_result: dict,
@@ -21121,9 +21143,13 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
             logger.debug("post-turn session resolution failed: %s", exc)
             return
 
-        # Empty interrupted/errored responses must not drive /goal, but an
-        # in-flight /loop tick still needs to be released and rescheduled.
-        if final_text.strip():
+        # A standing /goal may only continue after a genuinely successful turn.
+        # A provider failure is normalized into non-empty user-facing text, so
+        # gating on text alone would let the goal judge answer "continue" and
+        # loop on the error; require real success semantics. The /loop tick
+        # below still runs on every path so an in-flight tick is released and
+        # rescheduled even when the goal is held back.
+        if _should_continue_goal_after_turn(agent_result, final_text):
             try:
                 await self._post_turn_goal_continuation(
                     session_entry=session_entry,