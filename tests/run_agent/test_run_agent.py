NousResearch/hermes-agent

fix(agent): configurable empty-response retries + short backoff
The empty-response retry count was hardcoded to 3 with no backoff, so a
provider returning transient empty responses (e.g. z.ai GLM-5-Turbo,
observed ~2-3x/day) triggered premature fallback-provider switching. This
is not a quota/rate-limit signal (zero 429/529 events); the model simply
returns an empty response, and the immediate retries hit the same empty
state.

- agent._empty_response_retries (default 5, was hardcoded 3), configured
  via agent.empty_response_retries in config.yaml. 0 = skip retries and go
  straight to the fallback attempt.
- Add a short jittered backoff (jittered_backoff base_delay=3s, max_delay=15s)
  before the retry continue, interruptible with preserve_redirect parity to
  the 429/529 backoff (a steering redirect that lands during the wait is
  preserved and re-applied, with an iteration-budget refund so an exhausted
  budget does not strand it).

Tests: pin the existing retry-logic tests to 3, add a default-value test (5)
and a loop-at-default-5 boundary test. Docs: document the new key (EN + zh-Hans).

Ran /simplify and /code-review (high) on the diff before opening.

tests/run_agent/test_run_agent.py
@@ -2616,6 +2616,19 @@ class TestRunConversation:
         agent._use_prompt_caching = False
         agent.compression_enabled = False
         agent.save_trajectories = False
+        # Pin the empty-response retry count so the retry/fallback-logic tests
+        # below are deterministic and decoupled from the production default
+        # (agent._empty_response_retries, default 5; see
+        # test_empty_response_retries_default_is_5).
+        agent._empty_response_retries = 3
+
+    def test_empty_response_retries_default_is_5(self, agent):
+        """init_agent defaults _empty_response_retries to 5 (was hardcoded 3).
+
+        Deliberately does NOT call _setup_agent (which pins it to 3 for the
+        retry-logic tests), so the agent keeps its init_agent default.
+        """
+        assert agent._empty_response_retries == 5
 
     def test_task_start_failure_closes_relay_turn_and_lease(self, agent):
         relay_lease = SimpleNamespace(
@@ -3119,6 +3132,27 @@ class TestRunConversation:
         assert result["final_response"] != "(empty)"
         assert "No reply:" in result["final_response"]
         assert result["api_calls"] == 4  # 1 original + 3 retries
+
+    def test_truly_empty_response_retries_at_default_5(self, agent):
+        """Drives the loop at the production default (5), not the _setup_agent
+        pin of 3, so an off-by-one in the ``< _empty_response_retries`` boundary
+        is caught at the value users actually run."""
+        self._setup_agent(agent)
+        agent._empty_response_retries = 5  # the init_agent default
+        agent.base_url = "http://127.0.0.1:1234/v1"
+        empty_resp = _mock_response(content=None, finish_reason="stop")
+        # 6 responses: 1 original + 5 retries, all empty
+        agent.client.chat.completions.create.side_effect = [
+            empty_resp, empty_resp, empty_resp, empty_resp, empty_resp, empty_resp,
+        ]
+        with (
+            patch.object(agent, "_persist_session"),
+            patch.object(agent, "_save_trajectory"),
+            patch.object(agent, "_cleanup_task_resources"),
+        ):
+            result = agent.run_conversation("answer me")
+        assert result["completed"] is True
+        assert result["api_calls"] == 6  # 1 original + 5 retries
 
     def test_truly_empty_response_succeeds_on_nudge(self, agent):
         """Model produces content after being nudged for empty response."""

Carrie111998/Hermes

tests/run_agent/test_run_agent.py
@@ -2760,6 +2760,19 @@ class TestRunConversation:
         agent._use_prompt_caching = False
         agent.compression_enabled = False
         agent.save_trajectories = False
+        # Pin the empty-response retry count so the retry/fallback-logic tests
+        # below are deterministic and decoupled from the production default
+        # (agent._empty_response_retries, default 5; see
+        # test_empty_response_retries_default_is_5).
+        agent._empty_response_retries = 3
+
+    def test_empty_response_retries_default_is_5(self, agent):
+        """init_agent defaults _empty_response_retries to 5 (was hardcoded 3).
+
+        Deliberately does NOT call _setup_agent (which pins it to 3 for the
+        retry-logic tests), so the agent keeps its init_agent default.
+        """
+        assert agent._empty_response_retries == 5
 
     def test_task_start_failure_closes_relay_turn_and_lease(self, agent):
         relay_lease = SimpleNamespace(
@@ -3334,6 +3347,25 @@ class TestRunConversation:
             result = agent.run_conversation("answer me")
         assert result["completed"] is True
         assert result["api_calls"] == 4  # unchanged: 1 original + 3 retries
+
+    def test_truly_empty_response_retries_at_default_5(self, agent):
+        """Drives the loop at the production default (5), not the _setup_agent
+        pin of 3, so an off-by-one in the ``< _empty_retry_budget`` boundary
+        is caught at the value users actually run."""
+        self._setup_agent(agent)
+        agent._empty_response_retries = 5  # the init_agent default
+        agent.base_url = "http://127.0.0.1:1234/v1"
+        empty_resp = _mock_response(content=None, finish_reason="stop")
+        # 6 responses: 1 original + 5 retries, all empty
+        agent.client.chat.completions.create.side_effect = [empty_resp] * 6
+        with (
+            patch.object(agent, "_persist_session"),
+            patch.object(agent, "_save_trajectory"),
+            patch.object(agent, "_cleanup_task_resources"),
+        ):
+            result = agent.run_conversation("answer me")
+        assert result["completed"] is True
+        assert result["api_calls"] == 6  # 1 original + 5 retries
 
     def test_truly_empty_response_succeeds_on_nudge(self, agent):
         """Model produces content after being nudged for empty response."""