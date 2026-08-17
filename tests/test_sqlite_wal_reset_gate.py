NousResearch/hermes-agent

fix(state): share runtime-ownership repair resolver with doctor
Review feedback (#79232): the previous fix derived repairability from the
install-method label alone (git/unknown => not managed), which misread the
official installer's git clone — it carries a uv-managed venv, so hermes
update CAN rebuild the runtime there. Meanwhile hermes doctor still
promised "hermes update" for every non-Docker/non-Nix install, including
system-Python and unknown layouts where update cannot replace the linked
SQLite.

Add a single side-effect-free resolver, runtime_repair_capability(), in
hermes_cli/config.py that classifies by runtime ownership:
- docker / nix / nixos -> method-owned (image/derivation update)
- git/unknown WITH a checkout venv (venv or .venv) -> managed
- git/unknown WITHOUT one (system Python, pip --user) -> environment

Both consumers now use it: hermes_state._wal_reset_repair_hint() and
hermes_cli.doctor._sqlite_upgrade_hint(). Doctor no longer promises a
managed repair for environment-owned interpreters; managed installs keep
the hermes update guidance. Tests cover the full discriminating matrix.

tests/test_sqlite_wal_reset_gate.py
@@ -131,6 +131,10 @@ def test_doctor_warns_without_adding_issues(monkeypatch, tmp_path, capsys):
     )
     monkeypatch.setattr(hermes_state, "sqlite_source_id", lambda: "testid-abc")
     monkeypatch.setattr(sqlite3, "sqlite_version", "3.50.4", raising=False)
+    # A managed install (venv-owned runtime) keeps the `hermes update` path.
+    import hermes_cli.doctor as doctor
+
+    monkeypatch.setattr(doctor, "runtime_repair_capability", lambda _root: "managed")
 
     args = SimpleNamespace(fix=False, ack=None)
     try:
@@ -148,28 +152,31 @@ def test_doctor_warns_without_adding_issues(monkeypatch, tmp_path, capsys):
 
 
 class TestWalResetRepairHint:
-    """The WAL-reset repair hint must never promise that ``hermes update``
-    rebuilds the embedded SQLite runtime for installs whose interpreter is
-    not Hermes-managed (git checkout, pip, system Python, unknown) (#79179)."""
+    """The WAL-reset repair hint must match what ``hermes update`` can
+    actually repair, resolved by runtime ownership — not the install-method
+    label alone (#79179)."""
 
-    def _call_hint(self, method, cmd, monkeypatch):
+    def _call_hint(self, capability, cmd, monkeypatch):
         import hermes_cli.config as config
 
-        monkeypatch.setattr(config, "detect_install_method", lambda _root=None: method)
+        monkeypatch.setattr(
+            config, "runtime_repair_capability", lambda _root=None: capability
+        )
         monkeypatch.setattr(
             config, "recommended_update_command_for_method", lambda m: cmd
         )
         monkeypatch.setattr(config, "get_project_root", lambda: __import__("pathlib").Path("."))
         return hermes_state._wal_reset_repair_hint()
 
-    def test_git_checkout_never_promises_managed_update(self, monkeypatch):
-        hint = self._call_hint("git", "hermes update", monkeypatch)
-        assert "Hermes-managed" not in hint
-        assert "hermes update" not in hint
-        assert "SQLite" in hint
+    def test_managed_git_install_promises_managed_repair(self, monkeypatch):
+        """The official git clone carries a uv-managed venv — repairable."""
+        hint = self._call_hint("managed", "hermes update", monkeypatch)
+        assert "Hermes-managed" in hint
+        assert "hermes update" in hint
 
-    def test_unknown_install_never_promises_managed_update(self, monkeypatch):
-        hint = self._call_hint("unknown", "hermes update", monkeypatch)
+    def test_environment_owned_interpreter_never_promises_managed_update(self, monkeypatch):
+        """System Python / pip --user / unknown: the owner must upgrade."""
+        hint = self._call_hint("environment", "hermes update", monkeypatch)
         assert "Hermes-managed" not in hint
         assert "hermes update" not in hint
         assert "SQLite" in hint
@@ -182,4 +189,9 @@ class TestWalResetRepairHint:
     def test_managed_nix_still_returns_update_guidance(self, monkeypatch):
         hint = self._call_hint("nix", "nix run nousresearch/hermes-agent -- update", monkeypatch)
         assert "nix run" in hint
+        assert "Hermes-managed" not in hint
+
+    def test_nixos_still_returns_update_guidance(self, monkeypatch):
+        hint = self._call_hint("nixos", "nix profile upgrade", monkeypatch)
+        assert "nix profile upgrade" in hint
         assert "Hermes-managed" not in hint

Carrie111998/Hermes

tests/test_sqlite_wal_reset_gate.py
@@ -397,6 +397,15 @@ def test_doctor_warns_without_adding_issues(monkeypatch, tmp_path, capsys):
     )
     monkeypatch.setattr(hermes_state, "sqlite_source_id", lambda: "testid-abc")
     monkeypatch.setattr(sqlite3, "sqlite_version", "3.50.4", raising=False)
+    # A managed install (venv-owned runtime) keeps the `hermes update` path.
+    # Patch the resolver on hermes_cli.config (the single source of truth) —
+    # doctor._sqlite_upgrade_hint and hermes_state._wal_reset_repair_hint both
+    # resolve it fresh from config at call time.
+    import hermes_cli.config as config
+
+    monkeypatch.setattr(
+        config, "runtime_repair_capability", lambda _root=None: "managed"
+    )
 
     args = SimpleNamespace(fix=False, ack=None)
     try:
@@ -414,28 +423,31 @@ def test_doctor_warns_without_adding_issues(monkeypatch, tmp_path, capsys):
 
 
 class TestWalResetRepairHint:
-    """The WAL-reset repair hint must never promise that ``hermes update``
-    rebuilds the embedded SQLite runtime for installs whose interpreter is
-    not Hermes-managed (git checkout, pip, system Python, unknown) (#79179)."""
+    """The WAL-reset repair hint must match what ``hermes update`` can
+    actually repair, resolved by runtime ownership — not the install-method
+    label alone (#79179)."""
 
-    def _call_hint(self, method, cmd, monkeypatch):
+    def _call_hint(self, capability, cmd, monkeypatch):
         import hermes_cli.config as config
 
-        monkeypatch.setattr(config, "detect_install_method", lambda _root=None: method)
+        monkeypatch.setattr(
+            config, "runtime_repair_capability", lambda _root=None: capability
+        )
         monkeypatch.setattr(
             config, "recommended_update_command_for_method", lambda m: cmd
         )
         monkeypatch.setattr(config, "get_project_root", lambda: __import__("pathlib").Path("."))
         return hermes_state._wal_reset_repair_hint()
 
-    def test_git_checkout_never_promises_managed_update(self, monkeypatch):
-        hint = self._call_hint("git", "hermes update", monkeypatch)
-        assert "Hermes-managed" not in hint
-        assert "hermes update" not in hint
-        assert "SQLite" in hint
+    def test_managed_git_install_promises_managed_repair(self, monkeypatch):
+        """The official git clone carries a uv-managed venv — repairable."""
+        hint = self._call_hint("managed", "hermes update", monkeypatch)
+        assert "Hermes-managed" in hint
+        assert "hermes update" in hint
 
-    def test_unknown_install_never_promises_managed_update(self, monkeypatch):
-        hint = self._call_hint("unknown", "hermes update", monkeypatch)
+    def test_environment_owned_interpreter_never_promises_managed_update(self, monkeypatch):
+        """System Python / pip --user / unknown: the owner must upgrade."""
+        hint = self._call_hint("environment", "hermes update", monkeypatch)
         assert "Hermes-managed" not in hint
         assert "hermes update" not in hint
         assert "SQLite" in hint
@@ -448,4 +460,9 @@ class TestWalResetRepairHint:
     def test_managed_nix_still_returns_update_guidance(self, monkeypatch):
         hint = self._call_hint("nix", "nix run nousresearch/hermes-agent -- update", monkeypatch)
         assert "nix run" in hint
+        assert "Hermes-managed" not in hint
+
+    def test_nixos_still_returns_update_guidance(self, monkeypatch):
+        hint = self._call_hint("nixos", "nix profile upgrade", monkeypatch)
+        assert "nix profile upgrade" in hint
         assert "Hermes-managed" not in hint