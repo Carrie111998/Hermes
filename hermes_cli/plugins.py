lopiter/hermes-agent

fix(plugins): target inject_message at the originating dashboard session
inject_external_message routed every plugin notification to the most
recently active session. With two dashboard sessions open concurrently,
a dispatch completion report from session A landed in session B whenever
the user had typed there more recently — deterministically, not as a
race — and delivery bumped the wrong session's last_active, steering
every subsequent notification to it too.

Let callers pass the originating session: inject_external_message grows
target_sid, and PluginContext.inject_message grows session_id to carry
it. A targeted message is delivered to that session only — if it has
been closed it is dropped, never rerouted, because a report about
session A's work must not surface in session B's conversation. Untargeted
callers keep the most-recent routing.

Plugins capture the id inside the tool call via the session-context
ContextVar (HERMES_UI_SESSION_ID); watcher threads spawned later don't
inherit it, which is why the id travels by value.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

hermes_cli/plugins.py
@@ -473,7 +473,9 @@ class PluginContext:
 
     # -- message injection --------------------------------------------------
 
-    def inject_message(self, content: str, role: str = "user") -> bool:
+    def inject_message(
+        self, content: str, role: str = "user", session_id: str | None = None
+    ) -> bool:
         """Inject a message into the active conversation.
 
         If the agent is idle (waiting for user input), this starts a new turn.
@@ -481,6 +483,14 @@ class PluginContext:
 
         This enables plugins (e.g. remote control viewers, messaging bridges)
         to send messages into the conversation from external sources.
+
+        ``session_id`` is the dashboard session that originated the work
+        (capture it inside the tool call via
+        ``gateway.session_context.get_session_env("HERMES_UI_SESSION_ID")``
+        — watcher threads spawned later don't inherit the ContextVar). When
+        set, headless routing delivers to that session only and drops the
+        message if it has been closed; the interactive CLI ignores it, having
+        a single conversation.
 
         Returns True if the message was queued successfully.
         """
@@ -490,14 +500,17 @@ class PluginContext:
         if cli is None:
             # No interactive CLI in this process. The dashboard/desktop chat
             # hosts its sessions in the in-process tui_gateway server instead
-            # — route the message to its most recently active session. Gate on
+            # — route the message to the originating session when known,
+            # else to the most recently active one. Gate on
             # sys.modules: a process that never imported the dashboard stack
             # (e.g. a pure messaging gateway) has no sessions to route to, and
             # must not pay the import for a guaranteed miss.
             tui_server = sys.modules.get("tui_gateway.server")
             if tui_server is not None:
                 try:
-                    if tui_server.inject_external_message(msg):
+                    if tui_server.inject_external_message(
+                        msg, target_sid=session_id
+                    ):
                         return True
                 except Exception:
                     logger.warning(

Carrie111998/Hermes

hermes_cli/plugins.py
@@ -1969,6 +1969,7 @@ class PluginContext:
         self,
         content: str,
         role: str = "user",
+        session_id: str | None = None,
         *,
         session_key: str | None = None,
     ) -> bool:
@@ -1979,6 +1980,14 @@ class PluginContext:
 
         This enables plugins (e.g. remote control viewers, messaging bridges)
         to send messages into the conversation from external sources.
+
+        ``session_id`` is the dashboard session that originated the work
+        (capture it inside the tool call via
+        ``gateway.session_context.get_session_env("HERMES_UI_SESSION_ID")``
+        — watcher threads spawned later don't inherit the ContextVar). When
+        set, dashboard routing delivers to that session only and drops the
+        message if it has been closed; the interactive CLI ignores it, having
+        a single conversation.
 
         Gateway injection requires an existing ``session_key`` and an explicit
         ``plugins.entries.<plugin_id>.allow_gateway_injection`` config grant.
@@ -2001,14 +2010,15 @@ class PluginContext:
 
         # No interactive CLI in this process. The dashboard/desktop chat
         # hosts its sessions in the in-process tui_gateway server instead
-        # — route the message to its most recently active session. Gate on
-        # sys.modules: a process that never imported the dashboard stack
-        # (e.g. a pure messaging gateway) has no sessions to route to, and
-        # must not pay the import for a guaranteed miss.
+        # — route the message to the originating session when known, else
+        # to the most recently active one. Gate on sys.modules: a process
+        # that never imported the dashboard stack (e.g. a pure messaging
+        # gateway) has no sessions to route to, and must not pay the import
+        # for a guaranteed miss.
         tui_server = sys.modules.get("tui_gateway.server")
         if tui_server is not None:
             try:
-                if tui_server.inject_external_message(msg):
+                if tui_server.inject_external_message(msg, target_sid=session_id):
                     return True
             except Exception:
                 logger.warning(