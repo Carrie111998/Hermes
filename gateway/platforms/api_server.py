NousResearch/hermes-agent

fix(gateway): stream real reasoning_content instead of answer echo
The /v1/runs and /v1/chat/completions SSE paths showed the model's answer content (truncated to 500 chars) as the "thinking" block, so the reasoning panel was an exact echo of the response. Two coupled fixes:

1. agent/conversation_loop.py: top-level agents no longer emit reasoning.available from assistant_message.content. That fallback was meant for subagent delegation (a child's content IS the parent's reasoning surface) but was also firing for top-level agents, duplicating the answer as a fake chain-of-thought.

2. gateway/platforms/api_server.py: wire the agent's reasoning_callback (already fired by _fire_reasoning_delta from reasoning_content/reasoning deltas of GLM/DeepSeek/Kimi/Qwen thinking models) through to the SSE transports: /v1/runs emits reasoning.available events with the real thinking text; /v1/chat/completions emits delta.reasoning_content chunks. Non-thinking models never fire the callback, so no spurious reasoning block is shown.

Tested with GLM-5.2 via /v1/runs: reasoning panel now shows the model's chain-of-thought, distinct from the answer; non-thinking models show no reasoning block. Python syntax verified (py_compile).

gateway/platforms/api_server.py
@@ -2351,6 +2351,7 @@ class APIServerAdapter(BasePlatformAdapter):
         session_id: Optional[str] = None,
         stream_delta_callback=None,
         tool_progress_callback=None,
+        reasoning_callback=None,
         tool_start_callback=None,
         tool_complete_callback=None,
         gateway_session_key: Optional[str] = None,
@@ -2652,6 +2653,7 @@ class APIServerAdapter(BasePlatformAdapter):
             "platform": "api_server",
             "stream_delta_callback": stream_delta_callback,
             "tool_progress_callback": tool_progress_callback,
+            "reasoning_callback": reasoning_callback,
             "tool_start_callback": tool_start_callback,
             "tool_complete_callback": tool_complete_callback,
             "session_db": self._ensure_session_db(),
@@ -3858,7 +3860,7 @@ class APIServerAdapter(BasePlatformAdapter):
             _stream_q: _q.Queue = _q.Queue()
 
             def _on_delta(delta):
-                # Filter out None — the agent fires stream_delta_callback(None)
+                # Filter out None - the agent fires stream_delta_callback(None)
                 # to signal the CLI display to close its response box before
                 # tool execution, but the SSE writer uses None as end-of-stream
                 # sentinel.  Forwarding it would prematurely close the HTTP
@@ -3867,6 +3869,16 @@ class APIServerAdapter(BasePlatformAdapter):
                 # completion via agent_task.done() instead.
                 if delta is not None:
                     _stream_q.put(delta)
+
+            def _on_reasoning(text):
+                # Route the model's real chain-of-thought (reasoning_content
+                # delta) onto the SSE stream as delta.reasoning_content, tagged
+                # so _emit can distinguish it from content. Lets OpenAI-
+                # compatible clients that read delta.reasoning_content (GLM/
+                # DeepSeek/Kimi thinking models) render true reasoning instead
+                # of nothing or a content echo.
+                if text:
+                    _stream_q.put(("__reasoning__", text))
 
             # Track which tool_call_ids we've emitted a "running" lifecycle
             # event for, so a "completed" event without a matching "running"
@@ -3931,6 +3943,7 @@ class APIServerAdapter(BasePlatformAdapter):
                 ephemeral_system_prompt=system_prompt,
                 session_id=session_id,
                 stream_delta_callback=_on_delta,
+                reasoning_callback=_on_reasoning,
                 tool_start_callback=_on_tool_start,
                 tool_complete_callback=_on_tool_complete,
                 agent_ref=agent_ref,
@@ -4123,6 +4136,13 @@ class APIServerAdapter(BasePlatformAdapter):
                     await response.write(
                         f"event: hermes.tool.progress\ndata: {event_data}\n\n".encode()
                     )
+                elif isinstance(item, tuple) and len(item) == 2 and item[0] == "__reasoning__":
+                    reasoning_chunk = {
+                        "id": completion_id, "object": "chat.completion.chunk",
+                        "created": created, "model": model,
+                        "choices": [{"index": 0, "delta": {"reasoning_content": item[1]}, "finish_reason": None}],
+                    }
+                    await response.write(f"data: {json.dumps(reasoning_chunk)}\n\n".encode())
                 else:
                     content_chunk = {
                         "id": completion_id, "object": "chat.completion.chunk",
@@ -5762,6 +5782,7 @@ class APIServerAdapter(BasePlatformAdapter):
         session_id: Optional[str] = None,
         stream_delta_callback=None,
         tool_progress_callback=None,
+        reasoning_callback=None,
         tool_start_callback=None,
         tool_complete_callback=None,
         agent_ref: Optional[list] = None,
@@ -5821,6 +5842,7 @@ class APIServerAdapter(BasePlatformAdapter):
                         session_id=session_id,
                         stream_delta_callback=stream_delta_callback,
                         tool_progress_callback=tool_progress_callback,
+                        reasoning_callback=reasoning_callback,
                         tool_start_callback=tool_start_callback,
                         tool_complete_callback=tool_complete_callback,
                         gateway_session_key=gateway_session_key,
@@ -6206,6 +6228,28 @@ class APIServerAdapter(BasePlatformAdapter):
             except Exception:
                 pass
 
+        # Wire reasoning_callback so the model's real chain-of-thought
+        # (reasoning_content / reasoning delta field from GLM/DeepSeek/Kimi/
+        # Qwen thinking models) flows through as reasoning.available events.
+        # This replaces the old path that emitted content[:500] as a fake
+        # "thinking" block (conversation_loop no longer does that for
+        # top-level agents). Non-thinking models simply never fire this
+        # callback, so no spurious reasoning block is shown.
+        def _reasoning_cb(text: Optional[str]) -> None:
+            if not text:
+                return
+            if run_id not in self._run_streams:
+                return
+            try:
+                loop.call_soon_threadsafe(_put_event_if_active, {
+                    "event": "reasoning.available",
+                    "run_id": run_id,
+                    "timestamp": time.time(),
+                    "text": text,
+                })
+            except Exception:
+                pass
+
         self._set_run_status(
             run_id,
             "queued",
@@ -6239,6 +6283,7 @@ class APIServerAdapter(BasePlatformAdapter):
                         session_id=session_id,
                         stream_delta_callback=_text_cb,
                         tool_progress_callback=event_cb,
+                        reasoning_callback=_reasoning_cb,
                         gateway_session_key=gateway_session_key,
                         requested_model=agent_overrides.get("requested_model"),
                         requested_provider=agent_overrides.get("requested_provider"),

Carrie111998/Hermes

gateway/platforms/api_server.py
@@ -2781,6 +2781,7 @@ class APIServerAdapter(BasePlatformAdapter):
         session_id: Optional[str] = None,
         stream_delta_callback=None,
         tool_progress_callback=None,
+        reasoning_callback=None,
         tool_start_callback=None,
         tool_complete_callback=None,
         gateway_session_key: Optional[str] = None,
@@ -3094,6 +3095,7 @@ class APIServerAdapter(BasePlatformAdapter):
             "platform": "api_server",
             "stream_delta_callback": stream_delta_callback,
             "tool_progress_callback": tool_progress_callback,
+            "reasoning_callback": reasoning_callback,
             "tool_start_callback": tool_start_callback,
             "tool_complete_callback": tool_complete_callback,
             "session_db": self._ensure_session_db(),
@@ -5114,7 +5116,7 @@ class APIServerAdapter(BasePlatformAdapter):
             _stream_q = ThreadSafeAsyncQueue()
 
             def _on_delta(delta):
-                # Filter out None — the agent fires stream_delta_callback(None)
+                # Filter out None - the agent fires stream_delta_callback(None)
                 # to signal the CLI display to close its response box before
                 # tool execution, but the SSE writer uses None as end-of-stream
                 # sentinel.  Forwarding it would prematurely close the HTTP
@@ -5125,6 +5127,16 @@ class APIServerAdapter(BasePlatformAdapter):
                 # put_threadsafe (not put_nowait) is required here.
                 if delta is not None:
                     _stream_q.put_threadsafe(delta)
+
+            def _on_reasoning(text):
+                # Route the model's real chain-of-thought (reasoning_content
+                # delta) onto the SSE stream as delta.reasoning_content, tagged
+                # so _emit can distinguish it from content. Lets OpenAI-
+                # compatible clients that read delta.reasoning_content (GLM/
+                # DeepSeek/Kimi thinking models) render true reasoning instead
+                # of nothing or a content echo.
+                if text:
+                    _stream_q.put_threadsafe(("__reasoning__", text))
 
             # Track which tool_call_ids we've emitted a "running" lifecycle
             # event for, so a "completed" event without a matching "running"
@@ -5189,6 +5201,7 @@ class APIServerAdapter(BasePlatformAdapter):
                 ephemeral_system_prompt=system_prompt,
                 session_id=session_id,
                 stream_delta_callback=_on_delta,
+                reasoning_callback=_on_reasoning,
                 tool_start_callback=_on_tool_start,
                 tool_complete_callback=_on_tool_complete,
                 agent_ref=agent_ref,
@@ -5376,6 +5389,13 @@ class APIServerAdapter(BasePlatformAdapter):
                 """
                 if isinstance(item, tuple) and len(item) == 2 and item[0] == "__tool_progress__":
                     await response.write(_sse_frame(item[1], event="hermes.tool.progress"))
+                elif isinstance(item, tuple) and len(item) == 2 and item[0] == "__reasoning__":
+                    reasoning_chunk = {
+                        "id": completion_id, "object": "chat.completion.chunk",
+                        "created": created, "model": model,
+                        "choices": [{"index": 0, "delta": {"reasoning_content": item[1]}, "finish_reason": None}],
+                    }
+                    await response.write(_sse_frame(reasoning_chunk))
                 else:
                     content_chunk = {
                         "id": completion_id, "object": "chat.completion.chunk",
@@ -7128,6 +7148,7 @@ class APIServerAdapter(BasePlatformAdapter):
         session_id: Optional[str] = None,
         stream_delta_callback=None,
         tool_progress_callback=None,
+        reasoning_callback=None,
         tool_start_callback=None,
         tool_complete_callback=None,
         agent_ref: Optional[list] = None,
@@ -7203,6 +7224,7 @@ class APIServerAdapter(BasePlatformAdapter):
                         session_id=session_id,
                         stream_delta_callback=stream_delta_callback,
                         tool_progress_callback=tool_progress_callback,
+                        reasoning_callback=reasoning_callback,
                         tool_start_callback=tool_start_callback,
                         tool_complete_callback=tool_complete_callback,
                         gateway_session_key=gateway_session_key,
@@ -7617,6 +7639,28 @@ class APIServerAdapter(BasePlatformAdapter):
             except Exception:
                 pass
 
+        # Wire reasoning_callback so the model's real chain-of-thought
+        # (reasoning_content / reasoning delta field from GLM/DeepSeek/Kimi/
+        # Qwen thinking models) flows through as reasoning.available events.
+        # This replaces the old path that emitted content[:500] as a fake
+        # "thinking" block (conversation_loop no longer does that for
+        # top-level agents). Non-thinking models simply never fire this
+        # callback, so no spurious reasoning block is shown.
+        def _reasoning_cb(text: Optional[str]) -> None:
+            if not text:
+                return
+            if run_id not in self._run_streams:
+                return
+            try:
+                loop.call_soon_threadsafe(_put_event_if_active, {
+                    "event": "reasoning.available",
+                    "run_id": run_id,
+                    "timestamp": time.time(),
+                    "text": text,
+                })
+            except Exception:
+                pass
+
         self._set_run_status(
             run_id,
             "queued",
@@ -7656,6 +7700,7 @@ class APIServerAdapter(BasePlatformAdapter):
                         session_id=session_id,
                         stream_delta_callback=_text_cb,
                         tool_progress_callback=event_cb,
+                        reasoning_callback=_reasoning_cb,
                         gateway_session_key=gateway_session_key,
                         requested_model=agent_overrides.get("requested_model"),
                         requested_provider=agent_overrides.get("requested_provider"),