NousResearch/hermes-agent

feat(gateway): GatewayRunner loads registry, binds profile, propagates agent_id to hooks
GatewayRunner loads the agent registry at init and wraps every inbound
message in use_profile(). AIAgent accepts an optional profile= kwarg.
All invoke_hook call sites gain agent_id= kwarg. _handle_message is
split into _handle_message (ContextVar plumbing) + _handle_message_inner
(legacy logic) so tests that grep the source body continue to work.

- gateway/run.py: registry loading, use_profile() wrapping, hook kwargs
- run_agent.py: AIAgent(profile=), profile-aware model/toolset resolution
- model_tools.py, tools/{approval,terminal,delegate}.py: hook agent_id
- cli.py, tui_gateway/server.py: session boundary hook agent_id
- tests/gateway/test_profile_overrides.py: per-agent model/toolset overrides
- tests/test_model_tools.py: hook payload verification
- tests/gateway/test_{update,title,reasoning}_command.py: adapt to
  _handle_message split

tools/delegate_tool.py
@@ -2243,6 +2243,14 @@ def delegate_task(
         from hermes_cli.plugins import invoke_hook as _invoke_hook
     except Exception:
         _invoke_hook = None
+    _agent_id = None
+    try:
+        from agent.profile import get_active_profile
+        _p = get_active_profile()
+        if _p:
+            _agent_id = _p.id
+    except Exception:
+        pass
     # Aggregate child spend here so the parent's footer/UI reflect the true
     # cost of a subagent-heavy turn.  Port of Kilo-Org/kilocode#9448.  Each
     # child's cost was captured in _run_single_child before its AIAgent was
@@ -2267,6 +2275,7 @@ def delegate_task(
                 child_summary=entry.get("summary"),
                 child_status=entry.get("status"),
                 duration_ms=int((entry.get("duration_seconds") or 0) * 1000),
+                agent_id=_agent_id,
             )
         except Exception:
             logger.debug("subagent_stop hook invocation failed", exc_info=True)

Carrie111998/Hermes

tools/delegate_tool.py
@@ -3435,6 +3435,15 @@ def _finalize_child_results(
             from hermes_cli.plugins import invoke_hook as invoke_hook
         except Exception:
             invoke_hook = None
+        agent_id = None
+        try:
+            from agent.profile import get_active_profile
+
+            _p = get_active_profile()
+            if _p:
+                agent_id = _p.id
+        except Exception:
+            pass
 
         children_cost_total = 0.0
         for entry in results:
@@ -3462,6 +3471,7 @@ def _finalize_child_results(
                         entry.get("tool_trace")
                     ),
                     duration_ms=int((entry.get("duration_seconds") or 0) * 1000),
+                    agent_id=agent_id,
                 )
             except Exception:
                 logger.debug("subagent_stop hook invocation failed", exc_info=True)