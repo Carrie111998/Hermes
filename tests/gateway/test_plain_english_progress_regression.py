mrkillbob/hermes-agent

fix(gateway): restore plain-English project progress

tests/gateway/test_plain_english_progress_regression.py
@@ -1,0 +1,64 @@
+"""Regression coverage for natural-language Discord progress questions."""
+
+import pytest
+
+from hermes_cli import kanban_db as kb
+
+
+def test_plain_english_burndown_pr_question_is_a_progress_query():
+    try:
+        from gateway.progress_queries import is_progress_query
+    except ModuleNotFoundError:
+        pytest.fail("Discord progress-query routing is missing from the runtime")
+
+    assert is_progress_query(
+        "How’s the burndown patches going how much more do we have to do till we can send a PR"
+    )
+
+
+def test_plain_english_plural_burndown_question_summarizes_all_matching_roots(
+    tmp_path, monkeypatch
+):
+    from gateway.progress_queries import ProgressSource, resolve_progress_query
+
+    home = tmp_path / ".hermes"
+    home.mkdir()
+    monkeypatch.setenv("HERMES_HOME", str(home))
+    board = "tradingbot-burndown"
+    kb.init_db(board=board)
+    with kb.connect(board=board) as conn:
+        first = kb.create_task(
+            conn,
+            title="July exception burndown patches",
+            initial_status="running",
+            created_by="test",
+        )
+        second = kb.create_task(
+            conn,
+            title="August exception burndown patches",
+            initial_status="running",
+            created_by="test",
+        )
+        with kb.write_txn(conn):
+            conn.execute("UPDATE tasks SET status = 'done' WHERE id = ?", (first,))
+        for task_id in (first, second):
+            kb.add_notify_sub(
+                conn,
+                task_id=task_id,
+                platform="discord",
+                chat_id="trading-bot",
+                chat_type="group",
+                delivery_mode="notify",
+            )
+
+    result = resolve_progress_query(
+        "How’s the burndown patches going how much more do we have to do till we can send a PR",
+        source=ProgressSource(platform="discord", chat_id="trading-bot"),
+        board=board,
+    )
+
+    assert result.handled is True
+    assert result.reason == "resolved_multiple"
+    assert "July exception burndown patches" in result.response
+    assert "August exception burndown patches" in result.response
+    assert "Please name one" not in result.response

tests/gateway/test_specialist_handoff_orchestration.py
@@ -1,0 +1,112 @@
+"""Discord specialist work enters the durable orchestration lifecycle."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+from gateway.specialist_handoff import HandoffSource, create_specialist_handoff
+from gateway.specialist_routing import (
+    RouteKind,
+    SpecialistRouteDecision,
+    parse_specialist_response,
+)
+from hermes_cli import kanban_db as kb
+
+
+@pytest.fixture
+def kanban_home(tmp_path, monkeypatch):
+    home = tmp_path / ".hermes"
+    home.mkdir()
+    monkeypatch.setenv("HERMES_HOME", str(home))
+    monkeypatch.setattr(Path, "home", lambda: tmp_path)
+    kb.init_db()
+    return home
+
+
+def test_specialist_handoff_creates_goal_mode_triage_root(kanban_home):
+    decision = SpecialistRouteDecision(
+        kind=RouteKind.SPECIALIST,
+        profile="burndown-patch-steward",
+        confidence=1.0,
+        reason="explicit request",
+        title="Narrow Exception Burndown and Patching",
+    )
+    source = HandoffSource(
+        platform="discord",
+        chat_id="channel-1",
+        chat_type="group",
+        user_id="user-1",
+        message_id="message-1",
+    )
+
+    result = create_specialist_handoff(
+        decision=decision,
+        source=source,
+        request="Audit exception burndown and patch confirmed failures.",
+        board="tradingbot-burndown",
+    )
+
+    assert result.ok, result.reason
+    assert result.task_id
+    with kb.connect(board="tradingbot-burndown") as conn:
+        task = kb.get_task(conn, result.task_id)
+    assert task is not None
+    assert task.status == "triage"
+    assert task.goal_mode is True
+    assert task.goal_max_turns == 12
+    assert task.skills == ["tradingbot-worktree-navigation"]
+
+
+def test_specialist_handoff_explicit_board_ignores_database_environment_override(
+    kanban_home, monkeypatch, tmp_path
+):
+    decision = SpecialistRouteDecision(
+        kind=RouteKind.SPECIALIST,
+        profile="burndown-patch-steward",
+        confidence=1.0,
+        reason="explicit request",
+        title="Patch Exception Burndown",
+    )
+    source = HandoffSource(
+        platform="discord",
+        chat_id="channel-1",
+        chat_type="group",
+        user_id="user-1",
+        message_id="message-env-isolation",
+    )
+    board = "tradingbot-burndown"
+    with kb.connect(board=board):
+        pass
+    override_path = tmp_path / "override" / "kanban.db"
+    with kb.connect(db_path=override_path):
+        pass
+    monkeypatch.setenv("HERMES_KANBAN_DB", str(override_path))
+
+    result = create_specialist_handoff(
+        decision=decision,
+        source=source,
+        request="Patch the confirmed exception failures.",
+        board=board,
+    )
+
+    assert result.ok, result.reason
+    monkeypatch.delenv("HERMES_KANBAN_DB")
+    with kb.connect(board=board) as configured_conn:
+        configured_task = kb.get_task(configured_conn, result.task_id)
+    with kb.connect(db_path=override_path) as override_conn:
+        override_task = kb.get_task(override_conn, result.task_id)
+    assert configured_task is not None
+    assert override_task is None
+
+
+def test_router_accepts_task_orchestrator_for_broad_actionable_work():
+    decision = parse_specialist_response(
+        '{"kind":"specialist","profile":"task-orchestrator",'
+        '"confidence":0.91,"reason":"requires planning across roles",'
+        '"title":"Implement the requested workflow"}'
+    )
+
+    assert decision.dispatches is True
+    assert decision.profile == "task-orchestrator"

tests/hermes_cli/test_kanban_decompose_db.py
@@ -88,5 +88,55 @@ def test_decompose_records_audit_comment_and_event(kanban_home):
     assert any(ev.kind == "decomposed" for ev in events)
 
 
+def test_decompose_inherits_goal_lifecycle_to_children(kanban_home):
+    """A routed goal must not fan out into one-shot child workers."""
+    with kb.connect() as conn:
+        tid = kb.create_task(
+            conn,
+            title="durable orchestration root",
+            triage=True,
+            goal_mode=True,
+            goal_max_turns=12,
+        )
+        child_ids = kb.decompose_triage_task(
+            conn,
+            tid,
+            root_assignee="orchestrator",
+            children=[
+                {"title": "first slice", "assignee": "researcher"},
+                {"title": "second slice", "assignee": "engineer"},
+            ],
+            author="decomposer",
+        )
+    assert child_ids is not None
+
+    with kb.connect() as conn:
+        children = [kb.get_task(conn, child_id) for child_id in child_ids]
+
+    assert all(child is not None and child.goal_mode for child in children)
+    assert [child.goal_max_turns for child in children] == [12, 12]
+
+
+def test_decompose_inherits_forced_skills_to_children(kanban_home):
+    with kb.connect() as conn:
+        tid = kb.create_task(
+            conn,
+            title="navigation-bound root",
+            triage=True,
+            skills=["tradingbot-worktree-navigation"],
+        )
+        child_ids = kb.decompose_triage_task(
+            conn,
+            tid,
+            root_assignee="orchestrator",
+            children=[{"title": "first slice", "assignee": "researcher"}],
+        )
+    assert child_ids is not None
+
+    with kb.connect() as conn:
+        child = kb.get_task(conn, child_ids[0])
+
+    assert child is not None
+    assert child.skills == ["tradingbot-worktree-navigation"]
 
 

Carrie111998/Hermes

tests/gateway/test_plain_english_progress_regression.py
@@ -1,0 +1,64 @@
+"""Regression coverage for natural-language Discord progress questions."""
+
+import pytest
+
+from hermes_cli import kanban_db as kb
+
+
+def test_plain_english_burndown_pr_question_is_a_progress_query():
+    try:
+        from gateway.progress_queries import is_progress_query
+    except ModuleNotFoundError:
+        pytest.fail("Discord progress-query routing is missing from the runtime")
+
+    assert is_progress_query(
+        "How’s the burndown patches going how much more do we have to do till we can send a PR"
+    )
+
+
+def test_plain_english_plural_burndown_question_summarizes_all_matching_roots(
+    tmp_path, monkeypatch
+):
+    from gateway.progress_queries import ProgressSource, resolve_progress_query
+
+    home = tmp_path / ".hermes"
+    home.mkdir()
+    monkeypatch.setenv("HERMES_HOME", str(home))
+    board = "project-burndown"
+    kb.init_db(board=board)
+    with kb.connect(board=board) as conn:
+        first = kb.create_task(
+            conn,
+            title="July exception burndown patches",
+            initial_status="running",
+            created_by="test",
+        )
+        second = kb.create_task(
+            conn,
+            title="August exception burndown patches",
+            initial_status="running",
+            created_by="test",
+        )
+        with kb.write_txn(conn):
+            conn.execute("UPDATE tasks SET status = 'done' WHERE id = ?", (first,))
+        for task_id in (first, second):
+            kb.add_notify_sub(
+                conn,
+                task_id=task_id,
+                platform="discord",
+                chat_id="project-updates",
+                chat_type="group",
+                delivery_mode="notify",
+            )
+
+    result = resolve_progress_query(
+        "How’s the burndown patches going how much more do we have to do till we can send a PR",
+        source=ProgressSource(platform="discord", chat_id="project-updates"),
+        board=board,
+    )
+
+    assert result.handled is True
+    assert result.reason == "resolved_multiple"
+    assert "July exception burndown patches" in result.response
+    assert "August exception burndown patches" in result.response
+    assert "Please name one" not in result.response

tests/gateway/test_specialist_handoff_orchestration.py
@@ -1,0 +1,112 @@
+"""Discord specialist work enters the durable orchestration lifecycle."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+from gateway.specialist_handoff import HandoffSource, create_specialist_handoff
+from gateway.specialist_routing import (
+    RouteKind,
+    SpecialistRouteDecision,
+    parse_specialist_response,
+)
+from hermes_cli import kanban_db as kb
+
+
+@pytest.fixture
+def kanban_home(tmp_path, monkeypatch):
+    home = tmp_path / ".hermes"
+    home.mkdir()
+    monkeypatch.setenv("HERMES_HOME", str(home))
+    monkeypatch.setattr(Path, "home", lambda: tmp_path)
+    kb.init_db()
+    return home
+
+
+def test_specialist_handoff_creates_goal_mode_triage_root(kanban_home):
+    decision = SpecialistRouteDecision(
+        kind=RouteKind.SPECIALIST,
+        profile="burndown-patch-steward",
+        confidence=1.0,
+        reason="explicit request",
+        title="Narrow Exception Burndown and Patching",
+    )
+    source = HandoffSource(
+        platform="discord",
+        chat_id="channel-1",
+        chat_type="group",
+        user_id="user-1",
+        message_id="message-1",
+    )
+
+    result = create_specialist_handoff(
+        decision=decision,
+        source=source,
+        request="Audit exception burndown and patch confirmed failures.",
+        board="project-burndown",
+    )
+
+    assert result.ok, result.reason
+    assert result.task_id
+    with kb.connect(board="project-burndown") as conn:
+        task = kb.get_task(conn, result.task_id)
+    assert task is not None
+    assert task.status == "triage"
+    assert task.goal_mode is True
+    assert task.goal_max_turns == 12
+    assert task.skills is None
+
+
+def test_specialist_handoff_explicit_board_ignores_database_environment_override(
+    kanban_home, monkeypatch, tmp_path
+):
+    decision = SpecialistRouteDecision(
+        kind=RouteKind.SPECIALIST,
+        profile="burndown-patch-steward",
+        confidence=1.0,
+        reason="explicit request",
+        title="Patch Exception Burndown",
+    )
+    source = HandoffSource(
+        platform="discord",
+        chat_id="channel-1",
+        chat_type="group",
+        user_id="user-1",
+        message_id="message-env-isolation",
+    )
+    board = "project-burndown"
+    with kb.connect(board=board):
+        pass
+    override_path = tmp_path / "override" / "kanban.db"
+    with kb.connect(db_path=override_path):
+        pass
+    monkeypatch.setenv("HERMES_KANBAN_DB", str(override_path))
+
+    result = create_specialist_handoff(
+        decision=decision,
+        source=source,
+        request="Patch the confirmed exception failures.",
+        board=board,
+    )
+
+    assert result.ok, result.reason
+    monkeypatch.delenv("HERMES_KANBAN_DB")
+    with kb.connect(board=board) as configured_conn:
+        configured_task = kb.get_task(configured_conn, result.task_id)
+    with kb.connect(db_path=override_path) as override_conn:
+        override_task = kb.get_task(override_conn, result.task_id)
+    assert configured_task is not None
+    assert override_task is None
+
+
+def test_router_accepts_task_orchestrator_for_broad_actionable_work():
+    decision = parse_specialist_response(
+        '{"kind":"specialist","profile":"task-orchestrator",'
+        '"confidence":0.91,"reason":"requires planning across roles",'
+        '"title":"Implement the requested workflow"}'
+    )
+
+    assert decision.dispatches is True
+    assert decision.profile == "task-orchestrator"

tests/hermes_cli/test_kanban_decompose_db.py
@@ -88,5 +88,54 @@ def test_decompose_records_audit_comment_and_event(kanban_home):
     assert any(ev.kind == "decomposed" for ev in events)
 
 
+def test_decompose_inherits_goal_lifecycle_to_children(kanban_home):
+    """A routed goal must not fan out into one-shot child workers."""
+    with kb.connect() as conn:
+        tid = kb.create_task(
+            conn,
+            title="durable orchestration root",
+            triage=True,
+            goal_mode=True,
+            goal_max_turns=12,
+        )
+        child_ids = kb.decompose_triage_task(
+            conn,
+            tid,
+            root_assignee="orchestrator",
+            children=[
+                {"title": "first slice", "assignee": "researcher"},
+                {"title": "second slice", "assignee": "engineer"},
+            ],
+            author="decomposer",
+        )
+    assert child_ids is not None
 
+    with kb.connect() as conn:
+        children = [kb.get_task(conn, child_id) for child_id in child_ids]
+
+    assert all(child is not None and child.goal_mode for child in children)
+    assert [child.goal_max_turns for child in children] == [12, 12]
+
+
+def test_decompose_inherits_forced_skills_to_children(kanban_home):
+    with kb.connect() as conn:
+        tid = kb.create_task(
+            conn,
+            title="navigation-bound root",
+            triage=True,
+            skills=["project-worktree-navigation"],
+        )
+        child_ids = kb.decompose_triage_task(
+            conn,
+            tid,
+            root_assignee="orchestrator",
+            children=[{"title": "first slice", "assignee": "researcher"}],
+        )
+    assert child_ids is not None
+
+    with kb.connect() as conn:
+        child = kb.get_task(conn, child_ids[0])
+
+    assert child is not None
+    assert child.skills == ["project-worktree-navigation"]
 