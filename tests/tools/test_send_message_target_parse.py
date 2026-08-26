b454ab110d5c26de583bc02f61a8a4618ac4b061
arminanton <29869547+arminanton@users.noreply.github.com>
2026-08-26 04:19:40 -0700

    test(send): update target routing call contracts

--- tests/tools/test_send_message_target_parse.py
+++ tests/tools/test_send_message_target_parse.py
@@ -62,6 +62,7 @@ def test_send_message_routes_whatsapp_group_jid_without_home_fallback() -> None:
         thread_id=None,
         media_files=[],
         force_document=False,
+        force_plain=False,
     )
 
 
@@ -116,6 +117,7 @@ def test_resolved_opaque_plugin_target_uses_directory_id() -> None:
         thread_id=None,
         media_files=[],
         force_document=False,
+        force_plain=False,
     )
 
 
