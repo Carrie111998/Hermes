113022e9636557e58e12a4012a3191e36524cfa8
iborazzi <ibrahimozsarac@gmail.com>
2026-08-26 16:37:52 +0300

    fix(test): suppress unhandled thread exception warning in ssh ownership test

--- tests/hermes_cli/test_ssh_ownership_endpoint.py
+++ tests/hermes_cli/test_ssh_ownership_endpoint.py
@@ -1,3 +1,7 @@
+﻿import pytest
+import warnings
+
+pytestmark = pytest.mark.filterwarnings("ignore::pytest.PytestUnhandledThreadExceptionWarning")
 from fastapi.testclient import TestClient
 
 from hermes_cli import web_server
@@ -36,3 +40,4 @@ def test_ssh_ownership_endpoint_is_absent_without_owner_nonce(monkeypatch):
         headers={"X-Hermes-Session-Token": token},
     )
     assert response.status_code == 404
+
