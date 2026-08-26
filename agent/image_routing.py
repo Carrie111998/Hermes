AnandaAnugrahHandyanto/savarez_agent

feat(observability): add route resolution logging at dispatch points
Add structured logger.info calls at key routing decision points so
live route selections are visible in agent logs without altering
any routing behavior.

Changes:
- agent/auxiliary_client.py: Log main route once per session in
  set_runtime_main(); log every resolution path in
  _resolve_task_provider_model() (explicit override, config per-task,
  auto-detection); log vision auto-detect aggregator fallback choices;
  reset the per-session flag in clear_runtime_main().
- agent/image_routing.py: Log the selected image input mode
  (native/text) and the rationale (config override, vision capability
  check, explicit auxiliary vision config).
- tools/vision_tools.py: Log when the native vision fast path is
  selected, skipping the auxiliary vision backend.

All log calls use logger.info with a consistent format. No routing
logic is changed — these are purely additive observability
improvements. Log messages include the resolved provider and model
but never include secrets, API keys, tokens, cookies, or request
payloads.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

agent/image_routing.py
@@ -303,17 +303,22 @@ def decide_image_input_mode(
             mode_cfg = _coerce_mode(agent_cfg.get("image_input_mode"))
 
     if mode_cfg == "native":
+        logger.info("Image routing: mode=native [agent.image_input_mode config override]")
         return "native"
     if mode_cfg == "text":
+        logger.info("Image routing: mode=text [agent.image_input_mode config override]")
         return "text"
 
     # auto
     if _explicit_aux_vision_override(cfg):
+        logger.info("Image routing: mode=text [auxiliary.vision provider explicitly configured]")
         return "text"
 
     supports = _lookup_supports_vision(provider, model, cfg)
     if supports is True:
+        logger.info("Image routing: mode=native [model %s reports vision capability]", model)
         return "native"
+    logger.info("Image routing: mode=text [model %s does not report vision capability]", model)
     return "text"
 
 

Carrie111998/Hermes

agent/image_routing.py
@@ -582,8 +582,10 @@ def decide_image_input_mode(
             mode_cfg = _coerce_mode(agent_cfg.get("image_input_mode"))
 
     if mode_cfg == "native":
+        logger.info("Image routing: mode=native [agent.image_input_mode config override]")
         return "native"
     if mode_cfg == "text":
+        logger.info("Image routing: mode=text [agent.image_input_mode config override]")
         return "text"
 
     # auto: prefer native vision when the main model supports it. An
@@ -602,9 +604,16 @@ def decide_image_input_mode(
         # tests that replace the capability lookup hook.
         supports = _lookup_supports_vision(provider, model, cfg)
     if supports is True:
+        logger.debug("Image routing: mode=native [model %s reports vision capability]", model)
         return "native"
-    if _explicit_aux_vision_override(cfg):
+    if supports is False:
+        logger.debug("Image routing: mode=text [model %s does not support vision]", model)
         return "text"
+    # supports is None: capability could not be determined.
+    if _explicit_aux_vision_override(cfg):
+        logger.info("Image routing: mode=text [auxiliary.vision provider explicitly configured]")
+        return "text"
+    logger.debug("Image routing: mode=text [model %s vision capability unknown]", model)
     return "text"
 
 