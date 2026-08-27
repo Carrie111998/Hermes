NousResearch/hermes-agent

feat(api): surface agent context-window occupancy to thin clients
Add context_window and context_used columns to the sessions table,
a context_window_usage() helper that reads the agent compressor state,
and expose both fields through the API server session detail endpoint.
Thin clients (Hermes Uplink) can now display meaningful context usage
without computing it from cumulative token sums.

- hermes_state_common.py: add context_window/context_used columns (INTEGER
  NOT NULL DEFAULT 0) to SCHEMA_SQL; auto-added to existing DBs by
  _reconcile_columns()
- hermes_state.py: update_token_counts persists both fields with COALESCE
  no-clobber semantics in both absolute and incremental branches
- agent/context_breakdown.py: context_window_usage() reads compressor
  state, clamps -1 sentinel to 0, returns {0,0} for unknown
- agent/codex_runtime.py, agent/conversation_loop.py: pass
  **context_window_usage(agent) into update_token_counts
- gateway/platforms/api_server.py: _session_response exposes
  context_window and context_used in session detail payload

Co-authored-by: mr-september <45350177+mr-september@users.noreply.github.com>

hermes_state_common.py
@@ -230,6 +230,8 @@ CREATE TABLE IF NOT EXISTS sessions (
     cache_read_tokens INTEGER DEFAULT 0,
     cache_write_tokens INTEGER DEFAULT 0,
     reasoning_tokens INTEGER DEFAULT 0,
+    context_window INTEGER NOT NULL DEFAULT 0,
+    context_used INTEGER NOT NULL DEFAULT 0,
     cwd TEXT,
     git_branch TEXT,
     git_repo_root TEXT,

tests/agent/test_context_breakdown.py
@@ -2,7 +2,10 @@
 
 from unittest.mock import MagicMock, patch
 
-from agent.context_breakdown import compute_session_context_breakdown
+from agent.context_breakdown import (
+    compute_session_context_breakdown,
+    context_window_usage,
+)
 
 
 def _make_agent(
@@ -49,6 +52,54 @@ def test_breakdown_includes_major_categories():
     assert data["context_max"] == 200_000
     assert data["estimated_total"] > 0
 
+
+# ── context_window_usage ────────────────────────────────────────────────────
+
+
+def _agent_with_compressor(context_length=128_000, last_prompt_tokens=45_000):
+    """Build a mock agent whose compressor reports the given values."""
+    agent = MagicMock()
+    agent.context_compressor = MagicMock(
+        context_length=context_length,
+        last_prompt_tokens=last_prompt_tokens,
+    )
+    return agent
+
+
+def test_context_window_usage_returns_both_fields():
+    result = context_window_usage(_agent_with_compressor())
+    assert result == {"context_window": 128_000, "context_used": 45_000}
+
+
+def test_context_window_usage_clamps_negative_sentinel():
+    """The -1 "compression just ran" sentinel yields unknown (0,0)."""
+    result = context_window_usage(_agent_with_compressor(
+        context_length=128_000, last_prompt_tokens=-1,
+    ))
+    assert result == {"context_window": 0, "context_used": 0}
+
+
+def test_context_window_usage_no_compressor():
+    agent = MagicMock(spec=[])
+    result = context_window_usage(agent)
+    assert result == {"context_window": 0, "context_used": 0}
+
+
+def test_context_window_usage_compressor_missing_values():
+    agent = MagicMock()
+    agent.context_compressor = MagicMock(
+        context_length=0, last_prompt_tokens=0,
+    )
+    result = context_window_usage(agent)
+    assert result == {"context_window": 0, "context_used": 0}
+
+
+def test_context_window_usage_zero_window_yields_unknown():
+    """A context_length of 0 means 'not configured' -> both are 0."""
+    result = context_window_usage(_agent_with_compressor(
+        context_length=0, last_prompt_tokens=50_000,
+    ))
+    assert result == {"context_window": 0, "context_used": 0}
 
 
 # ── /context renderers (pure functions over the payload) ────────────────────

tests/test_hermes_state.py
@@ -396,6 +396,69 @@ class TestSessionLifecycle:
             db.close()
 
 
+class TestContextWindowPersistence:
+    """context_window / context_used columns in update_token_counts."""
+
+    def test_absolute_update_persists_both_fields(self, db):
+        db.create_session("s1", source="cli")
+        db.update_token_counts(
+            "s1", input_tokens=100, output_tokens=50, api_call_count=1,
+            absolute=True,
+            context_window=128_000, context_used=45_000,
+        )
+        row = db.get_session("s1")
+        assert row["context_window"] == 128_000
+        assert row["context_used"] == 45_000
+
+    def test_incremental_update_persists_context(self, db):
+        db.create_session("s1", source="cli")
+        db.update_token_counts(
+            "s1", input_tokens=100, output_tokens=50, api_call_count=1,
+            context_window=128_000, context_used=45_000,
+        )
+        row = db.get_session("s1")
+        assert row["context_window"] == 128_000
+        assert row["context_used"] == 45_000
+
+    def test_subsequent_update_without_context_does_not_clobber(self, db):
+        """COALESCE no-clobber: an update without context values preserves
+        the previous reading."""
+        db.create_session("s1", source="cli")
+        db.update_token_counts(
+            "s1", input_tokens=100, output_tokens=50, api_call_count=1,
+            context_window=128_000, context_used=45_000,
+        )
+        # Second update with no context_window / context_used
+        db.update_token_counts(
+            "s1", input_tokens=50, output_tokens=25, api_call_count=1,
+        )
+        row = db.get_session("s1")
+        assert row["context_window"] == 128_000
+        assert row["context_used"] == 45_000
+
+    def test_context_defaults_to_zero_fresh_session(self, db):
+        """A newly created session should have context_window=0, context_used=0."""
+        db.create_session("s1", source="cli")
+        row = db.get_session("s1")
+        assert row["context_window"] == 0
+        assert row["context_used"] == 0
+
+    def test_incremental_context_overwrites_prior_values(self, db):
+        """A later incremental update WITH context values should overwrite."""
+        db.create_session("s1", source="cli")
+        db.update_token_counts(
+            "s1", input_tokens=100, output_tokens=50, api_call_count=1,
+            context_window=128_000, context_used=45_000,
+        )
+        db.update_token_counts(
+            "s1", input_tokens=200, output_tokens=100, api_call_count=1,
+            context_window=256_000, context_used=120_000,
+        )
+        row = db.get_session("s1")
+        assert row["context_window"] == 256_000
+        assert row["context_used"] == 120_000
+
+
 # =========================================================================
 # Message storage
 # =========================================================================

Carrie111998/Hermes

hermes_state_common.py
@@ -392,6 +392,8 @@ CREATE TABLE IF NOT EXISTS sessions (
     cache_read_tokens INTEGER DEFAULT 0,
     cache_write_tokens INTEGER DEFAULT 0,
     reasoning_tokens INTEGER DEFAULT 0,
+    context_limit INTEGER NOT NULL DEFAULT 0,
+    context_used INTEGER NOT NULL DEFAULT 0,
     cwd TEXT,
     git_branch TEXT,
     git_repo_root TEXT,

tests/agent/test_context_breakdown.py
@@ -2,7 +2,10 @@
 
 from unittest.mock import MagicMock, patch
 
-from agent.context_breakdown import compute_session_context_breakdown
+from agent.context_breakdown import (
+    compute_session_context_breakdown,
+    context_window_usage,
+)
 
 
 def _make_agent(
@@ -49,6 +52,54 @@ def test_breakdown_includes_major_categories():
     assert data["context_max"] == 200_000
     assert data["estimated_total"] > 0
 
+
+# ── context_window_usage ────────────────────────────────────────────────────
+
+
+def _agent_with_compressor(context_length=128_000, last_prompt_tokens=45_000):
+    """Build a mock agent whose compressor reports the given values."""
+    agent = MagicMock()
+    agent.context_compressor = MagicMock(
+        context_length=context_length,
+        last_prompt_tokens=last_prompt_tokens,
+    )
+    return agent
+
+
+def test_context_window_usage_returns_both_fields():
+    result = context_window_usage(_agent_with_compressor())
+    assert result == {"context_limit": 128_000, "context_used": 45_000}
+
+
+def test_context_window_usage_clamps_negative_sentinel():
+    """The -1 "compression just ran" sentinel yields unknown (0,0)."""
+    result = context_window_usage(_agent_with_compressor(
+        context_length=128_000, last_prompt_tokens=-1,
+    ))
+    assert result == {"context_limit": 0, "context_used": 0}
+
+
+def test_context_window_usage_no_compressor():
+    agent = MagicMock(spec=[])
+    result = context_window_usage(agent)
+    assert result == {"context_limit": 0, "context_used": 0}
+
+
+def test_context_window_usage_compressor_missing_values():
+    agent = MagicMock()
+    agent.context_compressor = MagicMock(
+        context_length=0, last_prompt_tokens=0,
+    )
+    result = context_window_usage(agent)
+    assert result == {"context_limit": 0, "context_used": 0}
+
+
+def test_context_window_usage_zero_window_yields_unknown():
+    """A context_length of 0 means 'not configured' -> both are 0."""
+    result = context_window_usage(_agent_with_compressor(
+        context_length=0, last_prompt_tokens=50_000,
+    ))
+    assert result == {"context_limit": 0, "context_used": 0}
 
 
 # ── /context renderers (pure functions over the payload) ────────────────────

tests/test_hermes_state.py
@@ -512,6 +512,69 @@ class TestSessionLifecycle:
             db.close()
 
 
+class TestContextWindowPersistence:
+    """context_window / context_used columns in update_token_counts."""
+
+    def test_absolute_update_persists_both_fields(self, db):
+        db.create_session("s1", source="cli")
+        db.update_token_counts(
+            "s1", input_tokens=100, output_tokens=50, api_call_count=1,
+            absolute=True,
+            context_limit=128_000, context_used=45_000,
+        )
+        row = db.get_session("s1")
+        assert row["context_limit"] == 128_000
+        assert row["context_used"] == 45_000
+
+    def test_incremental_update_persists_context(self, db):
+        db.create_session("s1", source="cli")
+        db.update_token_counts(
+            "s1", input_tokens=100, output_tokens=50, api_call_count=1,
+            context_limit=128_000, context_used=45_000,
+        )
+        row = db.get_session("s1")
+        assert row["context_limit"] == 128_000
+        assert row["context_used"] == 45_000
+
+    def test_subsequent_update_without_context_does_not_clobber(self, db):
+        """COALESCE no-clobber: an update without context values preserves
+        the previous reading."""
+        db.create_session("s1", source="cli")
+        db.update_token_counts(
+            "s1", input_tokens=100, output_tokens=50, api_call_count=1,
+            context_limit=128_000, context_used=45_000,
+        )
+        # Second update with no context_window / context_used
+        db.update_token_counts(
+            "s1", input_tokens=50, output_tokens=25, api_call_count=1,
+        )
+        row = db.get_session("s1")
+        assert row["context_limit"] == 128_000
+        assert row["context_used"] == 45_000
+
+    def test_context_defaults_to_zero_fresh_session(self, db):
+        """A newly created session should have context_limit=0, context_used=0."""
+        db.create_session("s1", source="cli")
+        row = db.get_session("s1")
+        assert row["context_limit"] == 0
+        assert row["context_used"] == 0
+
+    def test_incremental_context_overwrites_prior_values(self, db):
+        """A later incremental update WITH context values should overwrite."""
+        db.create_session("s1", source="cli")
+        db.update_token_counts(
+            "s1", input_tokens=100, output_tokens=50, api_call_count=1,
+            context_limit=128_000, context_used=45_000,
+        )
+        db.update_token_counts(
+            "s1", input_tokens=200, output_tokens=100, api_call_count=1,
+            context_limit=256_000, context_used=120_000,
+        )
+        row = db.get_session("s1")
+        assert row["context_limit"] == 256_000
+        assert row["context_used"] == 120_000
+
+
 # =========================================================================
 # Message storage
 # =========================================================================