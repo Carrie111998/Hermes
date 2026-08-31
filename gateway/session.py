NousResearch/hermes-agent

fix(state): fail closed on ambiguous compression lineage

gateway/session.py
@@ -3674,24 +3674,34 @@ class SessionStore:
                 from hermes_state import CompressionSessionClosedError
 
                 if isinstance(exc, CompressionSessionClosedError):
-                    # Resolve the full continuation chain via the canonical
-                    # transitive API — a depth-1 live-child lookup misses
-                    # lineages with >=2 compression hops (root -> mid -> tip).
-                    # ``get_compression_tip`` returns the input id when no
-                    # continuation exists; adopt only a different, still-live
-                    # tip, otherwise fail closed as before.
-                    child_id = ""
-                    tip = self._db.get_compression_tip(session_id)
-                    if tip and tip != session_id:
-                        tip_row = self._db.get_session(tip)
-                        if tip_row is not None and tip_row.get("ended_at") is None:
-                            child_id = str(tip)
+                    # The conservative resolver validates the complete chain
+                    # and returns only its unique live canonical leaf.
+                    db: Any = self._db
+                    child = db.find_live_compression_child(session_id)
+                    child_id = (
+                        str(child["id"])
+                        if child and child.get("id")
+                        else ""
+                    )
                     if child_id:
+                        reroute_succeeded = False
                         try:
                             self._append_transcript_message(child_id, msg)
                         except Exception as reroute_exc:
                             exc = reroute_exc
+                            if (
+                                self._is_fts_corruption_error(reroute_exc)
+                                and self._rebuild_fts_once()
+                            ):
+                                try:
+                                    self._append_transcript_message(child_id, msg)
+                                except Exception as retry_exc:
+                                    exc = retry_exc
+                                else:
+                                    reroute_succeeded = True
                         else:
+                            reroute_succeeded = True
+                        if reroute_succeeded:
                             with self._transcript_retry_lock:
                                 if pending and pending[0] is msg:
                                     pending.pop(0)
@@ -3750,12 +3760,19 @@ class SessionStore:
                     except Exception as retry_exc:
                         exc = retry_exc
                     else:
+                        queue_empty = False
                         with self._transcript_retry_lock:
                             if pending and pending[0] is msg:
                                 pending.pop(0)
                             if not pending:
                                 self._dirty_transcripts.pop(queue_session_id, None)
                                 self._transcript_append_failures.pop(session_id, None)
+                                queue_empty = True
+                            else:
+                                msg = pending[0]
+                        if queue_empty:
+                            self._drain_spooled_drops(session_id)
+                            return
                         continue
                 with self._transcript_retry_lock:
                     failures = self._transcript_append_failures.get(session_id, 0) + 1

Carrie111998/Hermes

gateway/session.py
@@ -4005,30 +4005,19 @@ class SessionStore:
                     return
 
                 if isinstance(exc, CompressionSessionClosedError):
-                    # Resolve the full continuation chain via the canonical
-                    # transitive API — a depth-1 live-child lookup misses
-                    # lineages with >=2 compression hops (root -> mid -> tip).
-                    # ``get_compression_tip`` returns the input id when no
-                    # continuation exists; adopt only a different, still-live
-                    # tip, otherwise fail closed as before.
-                    #
-                    # The parent's id IS published in the routing index, so
-                    # its owner is already proven; the continuation's id is
-                    # not published until after the child write succeeds
-                    # (below), so resolving the child by id would miss and
-                    # fall back to the ambient store. Carry the proven handle
-                    # instead of re-deriving it from an id nothing points at
-                    # yet. ``_owner_db`` cannot be None here — the parent
-                    # append above just reached a real DB to raise this.
+                    # Resolve through the DB that owns the stale parent. The
+                    # conservative resolver validates the complete chain and
+                    # returns only its unique live canonical leaf.
                     _owner_key = self._owner_key_for_session_id(session_id)
-                    _owner_db = self._db_for_session_id(session_id)
+                    _owner_db: Any = self._db_for_session_id(session_id)
                     child_id = ""
                     if _owner_db is not None:
-                        tip = _owner_db.get_compression_tip(session_id)
-                        if tip and tip != session_id:
-                            tip_row = _owner_db.get_session(tip)
-                            if tip_row is not None and tip_row.get("ended_at") is None:
-                                child_id = str(tip)
+                        child = _owner_db.find_live_compression_child(session_id)
+                        child_id = (
+                            str(child["id"])
+                            if child and child.get("id")
+                            else ""
+                        )
                     if child_id:
                         # Record the child's owner BEFORE writing to it. The
                         # reroute and the _entries update are published only
@@ -4043,11 +4032,24 @@ class SessionStore:
                                 _hints = {}
                                 self._session_owner_hints = _hints
                             _hints[child_id] = _owner_key
+                        reroute_succeeded = False
                         try:
                             self._append_transcript_message(child_id, msg)
                         except Exception as reroute_exc:
                             exc = reroute_exc
+                            if (
+                                self._is_fts_corruption_error(reroute_exc)
+                                and self._rebuild_fts_once()
+                            ):
+                                try:
+                                    self._append_transcript_message(child_id, msg)
+                                except Exception as retry_exc:
+                                    exc = retry_exc
+                                else:
+                                    reroute_succeeded = True
                         else:
+                            reroute_succeeded = True
+                        if reroute_succeeded:
                             with self._transcript_retry_lock:
                                 if pending and pending[0] is msg:
                                     pending.pop(0)
@@ -4111,12 +4113,19 @@ class SessionStore:
                     except Exception as retry_exc:
                         exc = retry_exc
                     else:
+                        queue_empty = False
                         with self._transcript_retry_lock:
                             if pending and pending[0] is msg:
                                 pending.pop(0)
                             if not pending:
                                 self._dirty_transcripts.pop(queue_session_id, None)
                                 self._transcript_append_failures.pop(session_id, None)
+                                queue_empty = True
+                            else:
+                                msg = pending[0]
+                        if queue_empty:
+                            self._drain_spooled_drops(session_id)
+                            return
                         continue
                 with self._transcript_retry_lock:
                     failures = self._transcript_append_failures.get(session_id, 0) + 1