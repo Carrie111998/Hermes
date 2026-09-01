nuffin/hermes-agent

feat(session-topics): register /topic slash command with list/switch/new
Phase 9: CLI /topic command implementation.

- commands.py: repurpose /topic CommandDef for session topic management
  (was gateway-only Telegram DM topics). Now available in CLI.
- cli.py: dispatch 'topic' canonical → _handle_topic_command
- cli_commands_mixin.py: _handle_topic_command with three subcommands:
  list (default) — show all topics with active marker
  switch N — switch active topic to ID N
  new name — create a new topic

hermes_cli/cli_commands_mixin.py
@@ -852,6 +852,53 @@ class CLICommandsMixin:
         # and a no-op when the session recorded no cwd. See #38562.
         self._restore_session_cwd(session_meta)
 
+    def _handle_topic_command(self, cmd_original: str) -> None:
+        """Handle /topic [list|switch N|new name] — session topic management."""
+        from cli import _cprint
+        parts = cmd_original.split(None, 2)
+        sub = parts[1].strip().lower() if len(parts) > 1 else ""
+        arg = parts[2].strip() if len(parts) > 2 else ""
+
+        db = getattr(self, "_session_db", None)
+        sid = getattr(self, "session_id", None)
+
+        if not db or not sid:
+            _cprint("  Session database not available.")
+            return
+
+        if sub == "list" or not sub:
+            topics = db.get_topics(sid)
+            if not topics:
+                _cprint("  No topics in this session yet.")
+                return
+            _cprint("  Session Topics:")
+            for t in topics:
+                marker = " *" if t["state"] == "active" else "  "
+                _cprint(f"  {marker} [{t['id']}] {t['title']} ({t['message_count']} msgs, {t['state']})")
+
+        elif sub == "switch" or sub == "sw":
+            if not arg:
+                _cprint("  Usage: /topic switch <id>")
+                return
+            try:
+                tid = int(arg)
+            except ValueError:
+                _cprint(f"  Invalid topic id: {arg}")
+                return
+            if db.set_active_topic(sid, tid):
+                _cprint(f"  Switched to topic {tid}.")
+            else:
+                _cprint(f"  Topic {tid} not found.")
+
+        elif sub == "new":
+            name = arg if arg else "unnamed"
+            tid = db.create_topic(sid, name)
+            db.set_topic_session_title(sid)
+            _cprint(f"  Created topic [{tid}] '{name}'.")
+
+        else:
+            _cprint("  Usage: /topic [list|switch N|new name]")
+
     def _handle_sessions_command(self, cmd_original: str) -> None:
         """Handle /sessions [list|<id_or_title>] — browse or resume previous sessions.
 

Carrie111998/Hermes

hermes_cli/cli_commands_mixin.py
@@ -1303,6 +1303,53 @@ class CLICommandsMixin:
         # startup --resume (_preload_resumed_session / _init_agent path).
         self._restore_session_model(session_meta)
 
+    def _handle_topic_command(self, cmd_original: str) -> None:
+        """Handle /topic [list|switch N|new name] — session topic management."""
+        from cli import _cprint
+        parts = cmd_original.split(None, 2)
+        sub = parts[1].strip().lower() if len(parts) > 1 else ""
+        arg = parts[2].strip() if len(parts) > 2 else ""
+
+        db = getattr(self, "_session_db", None)
+        sid = getattr(self, "session_id", None)
+
+        if not db or not sid:
+            _cprint("  Session database not available.")
+            return
+
+        if sub == "list" or not sub:
+            topics = db.get_topics(sid)
+            if not topics:
+                _cprint("  No topics in this session yet.")
+                return
+            _cprint("  Session Topics:")
+            for topic in topics:
+                marker = " *" if topic["state"] == "active" else "  "
+                _cprint(f"  {marker} [{topic['id']}] {topic['title']} ({topic['message_count']} msgs, {topic['state']})")
+
+        elif sub == "switch" or sub == "sw":
+            if not arg:
+                _cprint("  Usage: /topic switch <id>")
+                return
+            try:
+                topic_id = int(arg)
+            except ValueError:
+                _cprint(f"  Invalid topic id: {arg}")
+                return
+            if db.set_active_topic(sid, topic_id):
+                _cprint(f"  Switched to topic {topic_id}.")
+            else:
+                _cprint(f"  Topic {topic_id} not found.")
+
+        elif sub == "new":
+            name = arg if arg else "unnamed"
+            topic_id = db.create_topic(sid, name)
+            db.set_topic_session_title(sid)
+            _cprint(f"  Created topic [{topic_id}] '{name}'.")
+
+        else:
+            _cprint("  Usage: /topic [list|switch N|new name]")
+
     def _handle_sessions_command(self, cmd_original: str) -> None:
         """Handle /sessions [list|<id_or_title>] — browse or resume previous sessions.
 