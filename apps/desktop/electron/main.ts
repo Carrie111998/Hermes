NousResearch/hermes-agent

fix(desktop): make the HUD retarget reachable from the toggle
Asking for HUD mode from a conversation the HUD is not on is supposed to
point the HUD at that conversation. Main already implements it —
openHudWindow() either sends `hermes:hud:goto` or respawns the window when
the target belongs to another profile — but nothing could ever reach it.

toggleHud() branched on $hudActive alone, so with the HUD open it always
took closeHud() and the `hermes:hud:open` IPC was never sent. $hudSession,
added alongside the retarget in f444e0c5e precisely to tell "switch the HUD
to this tab" apart from "dismiss the HUD", was written and never read. The
feature shipped dead: the toggle dismissed the HUD instead of switching it.

Branch on the target as well. Two cases still dismiss, deliberately: no
target to retarget onto, and the HUD's own window, where the toggle is the
way out (its renderer never subscribes to the state broadcast, so its
$hudSession is always null and every target would read as a retarget).

Main also has to broadcast a session switched from INSIDE the HUD. Every
window decides switch-vs-dismiss against its own copy of that id, and
`hermes:hud:session` updated main's copy silently — leaving the app window
comparing against the id the HUD was opened on, reading a retarget where
the user meant a dismiss.

apps/desktop/electron/main.ts
@@ -10215,7 +10215,19 @@ ipcMain.on('hermes:hud:move-by', (event, delta) => {
 // can hand it back to the app window (see hudSessionId).
 ipcMain.on('hermes:hud:session', (event, sessionId) => {
   if (hudWindow && !hudWindow.isDestroyed() && event.sender === hudWindow.webContents) {
-    hudSessionId = typeof sessionId === 'string' && sessionId ? sessionId : null
+    const next = typeof sessionId === 'string' && sessionId ? sessionId : null
+
+    if (next === hudSessionId) {
+      return
+    }
+
+    hudSessionId = next
+    // Every window decides "switch the HUD to this tab" vs "dismiss it" by
+    // comparing against its own copy of this id, so a conversation switched
+    // from INSIDE the HUD has to reach them too. Without this the app window
+    // keeps the id the HUD was opened on, and the toggle reads a retarget
+    // where the user meant a dismiss.
+    broadcastHudState(true)
   }
 })
 ipcMain.handle('hermes:hud:close', async () => {

Carrie111998/Hermes

apps/desktop/electron/main.ts
@@ -14675,7 +14675,17 @@ const hudIpc = registerHudIpc({
   closeHudWindow,
   resetHudLayout: resetHudWindowLayout,
   setHudSessionId: value => {
+    // Every window decides "switch the HUD to this tab" vs "dismiss it" by
+    // comparing against its own copy of this id, so a conversation switched
+    // from INSIDE the HUD has to reach them too. Without this the app window
+    // keeps the id the HUD was opened on, and the toggle reads a retarget
+    // where the user meant a dismiss.
+    if (value === hudSessionId) {
+      return
+    }
+
     hudSessionId = value
+    broadcastHudState(true)
   }
 })
 